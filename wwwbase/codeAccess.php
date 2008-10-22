7e8642772dec328bc870c6086054f501deaaf720
Cătălin Frâncu <cata@francu.com>
2008-10-22 23:31:14 +0000

    renamed cvsAccess to codeAccess

---
+++ wwwbase/codeAccess.php
@@ -0,0 +1,7 @@
+<?php
+require_once("../phplib/util.php");
+
+smarty_assign('page_title', 'DEX online - Acces la cod');
+smarty_assign('show_search_box', 0);
+smarty_displayCommonPageWithSkin('codeAccess.ihtml');
+?>

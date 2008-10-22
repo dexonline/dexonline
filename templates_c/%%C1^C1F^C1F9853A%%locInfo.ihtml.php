2c17a1248552b14597875482e2fbc303097b0271
Cătălin Frâncu <cata@francu.com>
2008-10-22 21:07:58 +0000

    removed templates_c

--- templates_c/%%C1^C1F^C1F9853A%%locInfo.ihtml.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2008-01-23 13:08:31
-         compiled from common/bits/locInfo.ihtml */ ?>
-<?php if ($this->_tpl_vars['lexem']->isLoc && $this->_tpl_vars['is_flex_moderator']): ?>
-  <span class="isLoc"
-        title="Inclus în lista oficială de cuvinte a jocului de scrabble"
-  >[LOC]</span>
-<?php endif; ?>      
--- templates_c/%%D3^D3E^D3E232A3%%helpLink.ihtml.php
+++
@@ -1,8 +0,0 @@
-<?php /* Smarty version 2.6.18, created on 2007-10-05 12:47:53
-         compiled from admin/helpLink.ihtml */ ?>
-<a href="#" onclick="adminHelpWindow('<?php echo $this->_tpl_vars['anchor']; ?>
-'); return false;">
-  <img src="<?php echo $this->_tpl_vars['imgRoot']; ?>
-/common/qmark.png" alt="ajutor" title="ajutor"
-       style="border: 0px"/>
-</a>

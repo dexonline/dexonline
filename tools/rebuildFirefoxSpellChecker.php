53e5993dab5d9ebaec381f98fecb15ab133be430
Cătălin Frâncu <cata@francu.com>
2008-12-27 06:58:07 +0000

    add debug info to rebuildFirefoxSpellChecker.php

--- tools/rebuildFirefoxSpellChecker.php
+++ tools/rebuildFirefoxSpellChecker.php
@@ -1,7 +1,9 @@
 <?
 require_once("../phplib/util.php");
 
+log_scriptLog('Running rebuildFirefoxSpellChecker.php.');
 $tmpDir = tempnam('/tmp', 'xpi_');
+log_scriptLog('Setting up directories');
 os_executeAndAssert("rm $tmpDir");
 os_executeAndAssert("mkdir $tmpDir");
 os_executeAndAssert("mkdir $tmpDir/chrome");
@@ -14,16 +16,20 @@ os_executeAndAssert("rm $mysqlFile");
 $query = "select distinct wl_neaccentuat from wordlist " .
   "where wl_neaccentuat rlike '^[a-zăâîşţ]+$' " .
   "into outfile '$mysqlFile'";
+log_scriptLog("Running mysql query: [$query]");
 mysql_query($query);
 
+log_scriptLog("Converting to ISO-8859-2");
 os_executeAndAssert("wc -l $mysqlFile | cut -d ' ' -f 1 " .
                     "> $tmpDir/dictionaries/ro-dex.dic");
 os_executeAndAssert("iconv -f UTF8 -t ISO_8859-2 $mysqlFile " .
                     ">> $tmpDir/dictionaries/ro-dex.dic");
 
+log_scriptLog("Zipping");
 os_executeAndAssert("cd $tmpDir && zip -r dex-ff.xpi *");
 os_executeAndAssert("cp -f $tmpDir/dex-ff.xpi ../wwwbase/download/");
 
 os_executeAndAssert("rm -rf $tmpDir");
+log_scriptLog('rebuildFirefoxSpellChecker.php completed successfully (against all odds)');
 
 ?>

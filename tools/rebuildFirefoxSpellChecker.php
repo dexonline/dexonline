e181cd13650679e64f7be09e3fc6420853cd5164
Cătălin Frâncu <cata@francu.com>
2009-07-24 17:34:51 +0000

    Fix firefox spell checker (now uses UTF8 and comma-below diacritics).

--- tools/rebuildFirefoxSpellChecker.php
+++ tools/rebuildFirefoxSpellChecker.php
@@ -8,7 +8,7 @@ os_executeAndAssert("rm $tmpDir");
 os_executeAndAssert("mkdir $tmpDir");
 os_executeAndAssert("mkdir $tmpDir/chrome");
 os_executeAndAssert("mkdir $tmpDir/dictionaries");
-os_executeAndAssert("echo 'SET ISO8859-2' > $tmpDir/dictionaries/ro-dex.aff");
+os_executeAndAssert("echo 'SET UTF-8' > $tmpDir/dictionaries/ro-dex.aff");
 os_executeAndAssert("cp ../docs/install.rdf $tmpDir/");
 
 $mysqlFile = tempnam('/tmp', 'mysql_');
@@ -19,11 +19,9 @@ $query = "select distinct wl_neaccentuat from wordlist " .
 log_scriptLog("Running mysql query: [$query]");
 mysql_query($query);
 
-log_scriptLog("Converting to ISO-8859-2");
-os_executeAndAssert("wc -l $mysqlFile | cut -d ' ' -f 1 " .
-                    "> $tmpDir/dictionaries/ro-dex.dic");
-os_executeAndAssert("iconv -f UTF8 -t ISO_8859-2 $mysqlFile " .
-                    ">> $tmpDir/dictionaries/ro-dex.dic");
+log_scriptLog("Prepending line count");
+os_executeAndAssert("wc -l $mysqlFile | cut -d ' ' -f 1 > $tmpDir/dictionaries/ro-dex.dic");
+os_executeAndAssert("cat $mysqlFile >> $tmpDir/dictionaries/ro-dex.dic");
 
 log_scriptLog("Zipping");
 os_executeAndAssert("cd $tmpDir && zip -r dex-ff.xpi *");

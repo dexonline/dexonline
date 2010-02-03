4107eb5bf71d3fce809a6dd41d13a09090e223cc
Radu Borza <radu_borza@yahoo.it>
2010-02-03 22:26:57 +0000

    fix queries for SLang dictionary

--- tools/2010-02-01/schemaChanges.sql
+++ tools/2010-02-01/schemaChanges.sql
@@ -1,5 +1,5 @@
 set names 'utf8';
 insert into Source values (26, 'Dicţionar argou', 'Dicţionar de argou al limbii române', 'George Volceanov', 'Editura Niculescu', '2006', 1, 1, 1, 26);
-update Definition set SourceId=26 where InternalRep like '%Niculescu, 2007%';
-update Definition set InternalRep=replace(InternalRep, substring(InternalRep, -96, 96), '') where SourceId = 26;
-update Definition set HtmlRep=replace(HtmlRep, substring(HtmlRep, -96, 96), '') where SourceId = 26;
+update Definition set SourceId=26 where InternalRep like '%Niculescu%';
+update Definition set InternalRep=replace(InternalRep, concat('(Not', substring_index(InternalRep, '(Not', -1)), '') where InternalRep like '%Niculescu%';
+update Definition set HtmlRep=replace(HtmlRep, concat('(Not', substring_index(HtmlRep, '(Not', -1)), '') where HtmlRep like '%Niculescu%';

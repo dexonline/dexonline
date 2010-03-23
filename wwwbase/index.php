01a41dcb47df9e9af1b0792540f622757e18ad01
Cătălin Frâncu <cata@francu.com>
2010-03-23 16:09:44 +0000

    Redirect after setting the skin. See #51.

--- wwwbase/index.php
+++ wwwbase/index.php
@@ -1,20 +1,18 @@
 <?php
 require_once("../phplib/util.php");
 
-// TODO: Change the skin in the session and redirect to /, don't use a GET arg.
 $skin = util_getRequestParameter('skin');
 
 if ($skin) {
   if (session_isValidSkin($skin)) {
     session_setSkin($skin);
   } else {
-    echo 'Numele skinului este incorect.';
-    return;
+    session_setFlash('Numele skinului este incorect.');
   }
+  util_redirect('index.php');
 }
 
-smarty_assign('page_title',
-	      'DEX online - Dicționar explicativ al limbii române');
+smarty_assign('page_title', 'DEX online - Dicționar explicativ al limbii române');
 smarty_assign('slick_selected', 'index');
 smarty_displayPageWithSkin('index.ihtml');
 ?>

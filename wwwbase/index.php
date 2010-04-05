70bcc6f90562ec7f07501afd1a7d1af67d569b10
Cătălin Frâncu <cata@francu.com>
2010-04-05 09:27:40 +0000

    Add Romanian diacritics to the word lists.

--- wwwbase/index.php
+++ wwwbase/index.php
@@ -3,6 +3,6 @@ require_once("../phplib/util.php");
 
 smarty_assign('page_title', 'Dicționar explicativ al limbii române');
 smarty_assign('hideZepuSmallLogo', '1');
-smarty_assign('letters', range('a', 'z'));
+smarty_assign('letters', preg_split('//u', 'aăâbcdefghiîjklmnopqrsștțuvwxyz'));
 smarty_displayPageWithSkin('index.ihtml');
 ?>
--- wwwbase/wordList.php
+++ wwwbase/wordList.php
@@ -2,7 +2,12 @@
 require_once("../phplib/util.php");
 
 $letter = util_getRequestParameter('letter');
-$forms = db_getArray(db_execute("select distinct formNoAccent from Lexem where formUtf8General like '{$letter}%' order by formNoAccent"));
+
+if (mb_strlen($letter) != '1') {
+  exit;
+}
+
+$forms = db_getArray(db_execute("select distinct formNoAccent from Lexem where formNoAccent like '{$letter}%' order by formNoAccent"));
 
 smarty_assign('forms', $forms);
 smarty_assign('letter', $letter);

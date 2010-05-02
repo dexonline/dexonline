2356a5c61965cf04dd042c8c1d72a14faf42e19d
Cătălin Frâncu <cata@francu.com>
2010-05-02 15:59:26 +0000

    - move small logo next to the search box - rename "hideZepuSmallLogo" to "onHomePage" since it may be useful   to other skins

--- wwwbase/index.php
+++ wwwbase/index.php
@@ -2,7 +2,7 @@
 require_once("../phplib/util.php");
 
 smarty_assign('page_title', 'Dicționar explicativ al limbii române');
-smarty_assign('hideZepuSmallLogo', '1');
+smarty_assign('onHomePage', '1');
 smarty_assign('letters', preg_split('//u', 'aăâbcdefghiîjklmnopqrsștțuvwxyz'));
 smarty_displayPageWithSkin('index.ihtml');
 ?>

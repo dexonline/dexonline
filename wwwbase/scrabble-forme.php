7feb5bacaea5e74d8c30d9780ec859393759dc35
Cătălin Frâncu <cata@francu.com>
2010-05-27 09:42:13 +0000

    Fix the unique scrabble form generator. It is now faster and uses less memory. Also cooks.

--- wwwbase/scrabble-forme.php
+++ wwwbase/scrabble-forme.php
@@ -6,29 +6,25 @@ ini_set("memory_limit", "128000000");
 $query = "select I.formNoAccent from InflectedForm I, Lexem L where I.lexemId = L.id and L.isLoc and char_length(I.formNoAccent) between 2 and 15";
 $dbResult = mysql_query($query);
 
-ob_start();
-
-while ($dbRow = mysql_fetch_row($dbResult)) {
-  print "{$dbRow[0]}\r\n";
-}
-
-$s = ob_get_contents();
-ob_end_clean();
-
-$s = text_unicodeToLatin(text_unicodeToUpper($s));
-
 $fileName = tempnam('/tmp', 'unique_');
-$fileName2 = tempnam('/tmp', 'unique_');
+$file = fopen($fileName, 'w');
+while ($dbRow = mysql_fetch_row($dbResult)) {
+  fwrite($file, "{$dbRow[0]}\r\n");
+}
+fclose($file);
+
+$s = file_get_contents($fileName);
+$s = text_unicodeToLatin($s);
+$s = strtoupper($s);
 $file = fopen($fileName, 'w');
 fwrite($file, $s);
 fclose($file);
 
+$fileName2 = tempnam('/tmp', 'unique_');
 os_executeAndAssert("sort -u $fileName -o $fileName2");
 
 header('Content-type: text/plain');
 print file_get_contents($fileName2);
-
 os_executeAndAssert("rm -f $fileName $fileName2");
-
 
 ?>

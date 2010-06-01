f3b6260dd4bdd22321eb53a0ec1904edfee05666
Cătălin Frâncu <cata@francu.com>
2010-06-01 10:21:47 +0000

    Add a user filter for the LOC lexem list.

--- wwwbase/flex/viewLocLexems.php
+++ wwwbase/flex/viewLocLexems.php
@@ -5,25 +5,36 @@ util_assertModerator(PRIV_LOC);
 util_assertNotMirror();
 
 $sourceId = util_getRequestParameter('source');
+$nick = util_getRequestParameter('nick');
 $loc = util_getRequestParameter('loc');
 
 $source = Source::get("id={$sourceId}");
 switch ($loc) {
   case 0:
-    $clause = " and not l.isLoc";
+    $srcClause = " and not l.isLoc";
     smarty_assign('sectionTitle', "Lexeme din {$source->shortName} neincluse în LOC");
     break;
   case 1:
-    $clause = " and l.isLoc";
+    $srcClause = " and l.isLoc";
     smarty_assign('sectionTitle', "Lexeme din {$source->shortName} incluse în LOC");
     break;
   case 2:
-    $clause = "";
+    $srcClause = "";
     smarty_assign('sectionTitle', "Lexeme din {$source->shortName}");
     break;
   default: exit;
 }
-$query = "select l.* from Lexem l, LexemDefinitionMap ldm, Definition d where l.id = ldm.lexemId and ldm.definitionId = d.id and d.status = 0 and d.sourceId = $sourceId $clause order by l.formNoAccent";
+
+$nickClause = '';
+if ($nick) {
+  $user = User::get("nick = '{$nick}'");
+  if ($user) {
+    $nickClause = "and d.userId = {$user->id}";
+  }
+}
+
+$query = "select l.* from Lexem l, LexemDefinitionMap ldm, Definition d where l.id = ldm.lexemId and ldm.definitionId = d.id and d.status = 0 and d.sourceId = $sourceId " .
+  "$nickClause $srcClause order by l.formNoAccent";
 $lexemDbResult = db_execute($query);
 
 smarty_assign('recentLinks', RecentLink::loadForUser());

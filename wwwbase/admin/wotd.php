ab86d5f0a2442d93dee56c851f5ca90d1a9d1f14
Radu Borza <radu_borza@yahoo.it>
2011-04-14 23:38:11 +0000

    fix wotd admin page

--- wwwbase/admin/wotd.php
+++ wwwbase/admin/wotd.php
@@ -6,6 +6,6 @@ RecentLink::createOrUpdate('Word of the Day');
 
 smarty_assign('sectionTitle', 'Word of the Day');
 smarty_assign('allStatuses', util_getAllStatuses());
-smarty_assign('recentLinks', RecentLink::loadForUser());
+//smarty_assign('recentLinks', RecentLink::loadForUser());
 smarty_displayWithoutSkin('admin/wotd.ihtml');
 ?>
--- wwwbase/js/jqgrid.init.js
+++ wwwbase/js/jqgrid.init.js
@@ -29,19 +29,19 @@ function initGrid(){
       datatype: 'xml',
       colNames: ['Source name', 'Lexicon', 'Defition HTML', 'User\'s name', 'Display data', 'Priority', 'WotD type', 'Definition ID'],
       colModel: [
-        {name: 'source', index: 'source', width: '120'},
-        {name: 'lexicon', index: 'lexicon', width: '120', editable: true},
-        {name: 'htmlRep', index: 'htmlRep', width: '120'},
-        {name: 'name', index: 'u.name', width: '120'},
-        {name: 'displayDate', index: 'displayDate', width: '120', editable: true},
-        {name: 'priority', index: 'priority', width: '120', editable: true},
-        {name: 'refType', index: 'refType', width: '120', editable: true, edittype: 'select', editoptions: {value: 'Definition:Definition'}}, 
-        {name: 'definitionId', index: 'definitionId', width: '120', editable: false, hidden: true}
+        {name: 'source', index: 'source', width: 100},
+        {name: 'lexicon', index: 'lexicon', editable: true},
+        {name: 'htmlRep', index: 'htmlRep', width: 600},
+        {name: 'name', index: 'u.name'},
+        {name: 'displayDate', index: 'displayDate', width: 100, editable: true},
+        {name: 'priority', index: 'priority', editable: true, width: 80},
+        {name: 'refType', index: 'refType', width: 100, editable: true, edittype: 'select', editoptions: {value: 'Definition:Definition'}}, 
+        {name: 'definitionId', index: 'definitionId', editable: false, hidden: true}
       ],
-      rowNum: 25,
-      //autoWidth: true,
-      width: 1200,
-      rowList: [25, 50, 75],
+      rowNum: 50,
+      autoWidth: true,
+      height: '100%',
+      rowList: [25, 50, 100, 200],
       sortName: 'displayDate',
       pager: $('#wotdPaging'), 
       viewRecords: true,
@@ -52,7 +52,6 @@ function initGrid(){
     $('#wotdGrid').navGrid('#wotdPaging', {},
       {
         reloadAfterSubmit: true,
-        width: 600,
         beforeSubmit: function(data){
           return beforeSubmit(data);
         },
@@ -62,7 +61,6 @@ function initGrid(){
       },
       {
         reloadAfterSubmit: true,
-        width: 600,
         beforeSubmit: function(data){
           return beforeSubmit(data);
         },

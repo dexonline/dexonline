38f47e32a4c29f25e1a02907a60337e619232d52
Cătălin Frâncu <cata@francu.com>
2011-09-27 21:44:20 +0000

    add a script to kill runaway php processes

---
+++ tools/killOrphanPhpProcesses.php
@@ -0,0 +1,30 @@
+<?
+require_once("../phplib/util.php");
+
+$PS_COMMAND = 'ps -eo user,pid,etime,args --no-headers --sort etime';
+$APACHE_USER = 'www-data';
+$PHP_EXECUTABLE = '/usr/lib/cgi-bin/php5';
+$TIME_LIMIT = 3600; /*seconds */
+
+log_scriptLog('Running killOrphanPhpProcesses.php.');
+$output = os_executeAndReturnOutput($PS_COMMAND);
+foreach($output as $line) {
+  $parts = preg_split('/ +/', $line, 4);
+  $runningTime = getRunningTime($parts[2]);
+  if ($parts[0] == $APACHE_USER && $runningTime > $TIME_LIMIT && $parts[3] == $PHP_EXECUTABLE) {
+    log_scriptLog("killing process {$parts[1]}\n");
+    // os_executeAndAssert("kill -9 {$parts[1]}\n");
+  }
+}
+log_scriptLog('killOrphanPhpProcesses.php done.');
+
+/****************************************************************************/
+
+// ps gives us the running time in [[DD-]hh:]mm:ss format.
+function getRunningTime($string) {
+  $matches = array();
+  preg_match("/^(?:(?:(\\d+)-)?(\\d+):)?(\\d+):(\\d+)$/", $string, $matches);
+  return $matches[1] * 86400 + $matches[2] * 3600 + $matches[3] * 60 + $matches[4];
+}
+
+?>

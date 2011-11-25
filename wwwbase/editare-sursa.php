7168bdebdc9cc8f684c27d0ae6571bcd53c931bc
Cătălin Frâncu <cata@francu.com>
2011-11-25 14:09:24 +0000

    Simplify source viewing and editing: - Remove source listing from the FAQ. - Make the /surse page public - Sources can only be viewed (and reorderd by admins) at /surse. They can   no longer be edited there. - Admins can drill down into each source and edit it on a separate page. Addresses #236.

---
+++ wwwbase/editare-sursa.php
@@ -0,0 +1,48 @@
+<?php
+require_once("../phplib/util.php");
+util_assertModerator(PRIV_ADMIN);
+
+$sourceId = util_getRequestParameter('id');
+$submitButton = util_getRequestParameter('submitButton');
+$src = $sourceId ? Source::get_by_id($sourceId) : Model::factory('Source')->create();
+
+if ($submitButton) {
+  $src->name = util_getRequestParameter("name");
+  $src->shortName = util_getRequestParameter("shortName");
+  $src->urlName = util_getRequestParameter("urlName");
+  $src->author = util_getRequestParameter("author");
+  $src->publisher = util_getRequestParameter("publisher");
+  $src->year = util_getRequestParameter("year");
+  $src->isOfficial = util_getRequestParameterWithDefault("isOfficial", 0);
+  $src->canContribute = util_getRequestParameterWithDefault("canContribute", 0);
+  $src->canModerate = util_getRequestParameterWithDefault("canModerate", 0);
+  $src->canDistribute = util_getRequestParameterWithDefault("canDistribute", 0);
+
+  if (validate($src)) {
+    // For new sources, set displayOrder to the highest available + 1
+    if (!$sourceId) {
+      $src->displayOrder = Model::factory('Source')->count() + 1;
+    }
+    $src->save();
+    FlashMessage::add('Modificările au fost salvate', 'info');
+    util_redirect("editare-sursa?id={$src->id}");
+  }
+}
+
+smarty_assign('src', $src);
+smarty_assign('page_title', $sourceId ? "Editare sursă {$src->shortName}" : "Adăugare sursă");
+smarty_displayCommonPageWithSkin('editare-sursa.ihtml');
+
+/**
+ * Returns true on success, false on errors.
+ */
+function validate($src) {
+  $success = true;
+  if (!$src->name) { FlashMessage::add('Numele nu poate fi vid.'); $success = false; }
+  if (!$src->shortName) { FlashMessage::add('Numele scurt nu poate fi vid.'); $success = false; }
+  if (!$src->urlName) { FlashMessage::add('Numele URL nu poate fi vid.'); $success = false; }
+  if (!$src->author) { FlashMessage::add('Autorul nu poate fi vid.'); $success = false; }
+  return $success;
+}
+
+?>
--- wwwbase/surse.php
+++ wwwbase/surse.php
@@ -1,59 +1,23 @@
 <?php
 require_once("../phplib/util.php");
-util_assertModerator(PRIV_ADMIN);
 
 $submitButton = util_getRequestParameter('submitButton');
-$moveUp = util_getRequestParameter('moveUp');
-$moveDown = util_getRequestParameter('moveDown');
-$ids = util_getRequestParameter('ids');
-
-if ($moveUp) {
-  switchSources($moveUp, $moveUp - 1);
-  util_redirect('surse');
-}
-
-if ($moveDown) {
-  switchSources($moveDown, $moveDown + 1);
-  util_redirect('surse');
-}
 
 if ($submitButton) {
-  foreach($ids as $id) {
-    $src = ($id == 'new') ? Model::factory('Source')->create() : Source::get_by_id($id);
-    $src->name = util_getRequestParameter("name_{$id}");
-    $src->shortName = util_getRequestParameter("shortName_{$id}");
-    $src->urlName = util_getRequestParameter("urlName_{$id}");
-    $src->author = util_getRequestParameter("author_{$id}");
-    $src->publisher = util_getRequestParameter("publisher_{$id}");
-    $src->year = util_getRequestParameter("year_{$id}");
-    $src->canContribute = util_getRequestParameterWithDefault("canContribute_{$id}", 0);
-    $src->canModerate = util_getRequestParameterWithDefault("canModerate_{$id}", 0);
-    $src->isOfficial = util_getRequestParameterWithDefault("isOfficial_{$id}", 0);
-    $src->canDistribute = util_getRequestParameterWithDefault("canDistribute_{$id}", 0);
-    if ($src->name) {
-      if ($id == 'new') {
-        $src->displayOrder = count($ids); // set the highest displayOrder for the new source
-      }
-      $src->save();
-    }
+  util_assertModerator(PRIV_ADMIN);
+  $order = 1;
+  $ids = util_getRequestParameter("ids");
+  foreach ($ids as $id) {
+    $src = Source::get_by_id($id);
+    $src->displayOrder = $order++;
+    $src->save();
   }
+  FlashMessage::add('Ordinea a fost salvată.', 'info');
   util_redirect('surse');
 }
 
-// Note that we do NOT sort sources by isOfficial here, otherwise reordering will not work.
 smarty_assign('sources', Model::factory('Source')->order_by_asc('displayOrder')->find_many());
 smarty_assign('page_title', 'Surse');
 smarty_displayCommonPageWithSkin('surse.ihtml');
-
-function switchSources($ord1, $ord2) {
-  $src1 = Source::get_by_displayOrder($ord1);
-  $src2 = Source::get_by_displayOrder($ord2);
-  if ($src1 && $src2) {
-    $src1->displayOrder = $ord2;
-    $src2->displayOrder = $ord1;
-    $src1->save();
-    $src2->save();
-  }
-}
 
 ?>

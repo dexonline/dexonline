1d4f440c2b69e130a832bd701e33cf153b0079ba
Cătălin Frâncu <cata@francu.com>
2012-09-15 10:47:48 +0000

    Optimize the genNGram script by using multiple inserts. Drop unused indices on the NGram table.

---
+++ patches/00073.sql
@@ -0,0 +1,2 @@
+drop index ngram on NGram;
+drop index pos on NGram;
--- tools/genNGram.php
+++ tools/genNGram.php
@@ -1,47 +1,43 @@
 <?php
 
-include_once "../phplib/util.php";
+require_once __DIR__ . '/../phplib/util.php';
+define('INSERT_SIZE', 10000);
 
 ini_set('max_execution_time', '3600');
 
-$time = explode(" ", microtime());
-$start = $time[1] + $time[0];
-
-db_execute("delete from NGram");
-db_execute("ALTER TABLE NGram AUTO_INCREMENT = 1");
-print "Table content deleted\n" ;
-
-$time = explode(" ", microtime());
-$end1 = $time[1] + $time[0];
-$diff_time = sprintf('%0.5f', $end1 - $start);
-print "Time to delete content:  $diff_time seconds\n";
+log_scriptLog('Running genNGram.php.');
+$start = microtime(true);
+db_execute("truncate table NGram"); // This should be fast
 
 $dbResult = db_execute("select * from Lexem", PDO::FETCH_ASSOC);
 
-$id = 1;
+$values = array();
 foreach ($dbResult as $cnt => $row) {
   $lexem = Model::factory('Lexem')->create($row);
   $form = NGram::padWord($lexem->formNoAccent);
-	$len = mb_strlen($form);
+  $len = mb_strlen($form);
 
-	for ($i = 0; $i < $len - NGram::$NGRAM_SIZE + 1; $i++) {
-		$n = Model::factory('NGram')->create();
-		$n->ngram = mb_substr($form, $i, NGram::$NGRAM_SIZE);
-		$n->pos = $i;
-		$n->lexemId = $lexem->id;
-		$n->save();
-		$id++;
-	}
+  for ($i = 0; $i < $len - NGram::$NGRAM_SIZE + 1; $i++) {
+    $values[] = array(addslashes(mb_substr($form, $i, NGram::$NGRAM_SIZE)), $i, $lexem->id);
+  }
+  if (count($values) >= INSERT_SIZE) {
+    // Assemble low-level MySQL query. Idiorm inserts records one by one, which is many times slower.
+    $query = 'insert into NGram(ngram, pos, lexemId) values ';
+    foreach ($values as $i => $set) {
+      if ($i) {
+	$query .= ',';
+      }
+      $query .= sprintf("('%s', %d, %d)", $set[0], $set[1], $set[2]);
+    }
+    db_execute($query);
+    $values = array();
+  }
   if ($cnt % 1000 == 0) {
-    print "$cnt lexems processed\n" ;
+    log_scriptLog(sprintf("%d lexems processed, %0.3f lexems/second.", $cnt, $cnt / (microtime(true) - $start)));
   }
 }
 
-$time = explode(" ", microtime());
-$end2 = $time[1] + $time[0];
-$diff_time = sprintf('%0.5f', $end2 - $end1);
-print "Time to create NGram table:  $diff_time seconds\n";
-
-print "$id lines created\n";
+$end = microtime(true);
+log_scriptLog(sprintf("genNGram.php completed in %0.3f seconds\n", $end - $start));
 
 ?>

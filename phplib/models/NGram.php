ec2b81c7cc6489e3b1dca11509c25c5187707f72
Cătălin Frâncu <cata@francu.com>
2012-09-15 12:42:45 +0000

    For N-grams, move the splitting login in a common function split().

--- phplib/models/NGram.php
+++ phplib/models/NGram.php
@@ -2,47 +2,51 @@
 
 class NGram extends BaseObject {
 
-	public static $_table = 'NGram';
-	public static $NGRAM_SIZE = 3;
-	public static $MAX_MOVE = 2; // Maximum distance an n-gram is allowed to migrate
-	public static $LENGTH_DIF = 2; // Maximum length difference between the searched word and the suggested one
+  public static $_table = 'NGram';
+  public static $NGRAM_SIZE = 3;
+  public static $MAX_MOVE = 2; // Maximum distance an n-gram is allowed to migrate
+  public static $LENGTH_DIF = 2; // Maximum length difference between the searched word and the suggested one
 
-	public static function padWord($s) {
-		return str_repeat('#', self::$NGRAM_SIZE - 1) . $s . str_repeat('%', self::$NGRAM_SIZE - 1);
-	}
+  // Returns an array of n-grams.
+  public static function split($s) {
+    $s = str_repeat('#', self::$NGRAM_SIZE - 1) . $s . str_repeat('%', self::$NGRAM_SIZE - 1);
+    $len = mb_strlen($s);
+    $results = array();
+    for ($i = 0; $i < $len - self::$NGRAM_SIZE + 1; $i++) {
+      $results[] = mb_substr($s, $i, self::$NGRAM_SIZE);
+    }
+    return $results;
+  }
 
-	public static function searchNGram($cuv) {
-		$leng = mb_strlen($cuv);
-		$ngramList = array();
-		$ext = self::padWord($cuv);
-		for ($i = 0; $i < $leng + self::$NGRAM_SIZE - 1; $i++)
-			array_push($ngramList, mb_substr($ext,$i,self::$NGRAM_SIZE));
-		$i = 0;
-		$hash = array();
-		foreach($ngramList as $ngram) {
-			$lexemIdList = db_getArray(
-				sprintf("select lexemId from NGram where ngram = '%s' and pos between %d and %d",
-					$ngram, $i - self::$MAX_MOVE, $i + self::$MAX_MOVE));
-			$lexemIdList = array_unique($lexemIdList);
-			foreach($lexemIdList as $lexemId) {
-				if (!isset($hash[$lexemId]))
-					$hash[$lexemId] = 1;
-				else
-					$hash[$lexemId]++;
-			}
-			$i++;
-		}
-		arsort($hash);
-		$max = current($hash);
-		$lexIds = array_keys($hash,$max);
-		$finalResult = array();
-		foreach ($lexIds as $id) {
-			$result = Model::factory('Lexem')->where('id', $id)->where_gte('charLength', $leng - self::$LENGTH_DIF)->where_lte('charLength', $leng + self::$LENGTH_DIF)->find_one();
-			if ($result)
-				array_push($finalResult, $result);
-		}
-		return $finalResult;
+  public static function searchNGram($cuv) {
+    $leng = mb_strlen($cuv);
+    $ngramList = self::split($cuv);
+    $hash = array();
+    foreach($ngramList as $i => $ngram) {
+      $lexemIdList = db_getArray(sprintf("select lexemId from NGram where ngram = '%s' and pos between %d and %d",
+                                         $ngram, $i - self::$MAX_MOVE, $i + self::$MAX_MOVE));
+      $lexemIdList = array_unique($lexemIdList);
+      foreach($lexemIdList as $lexemId) {
+        if (!isset($hash[$lexemId])) {
+          $hash[$lexemId] = 1;
+        } else {
+          $hash[$lexemId]++;
 	}
+      }
+    }
+    arsort($hash);
+    $max = current($hash);
+    $lexIds = array_keys($hash,$max);
+    $finalResult = array();
+    foreach ($lexIds as $id) {
+      $result = Model::factory('Lexem')->where('id', $id)->where_gte('charLength', $leng - self::$LENGTH_DIF)
+	->where_lte('charLength', $leng + self::$LENGTH_DIF)->find_one();
+      if ($result) {
+        array_push($finalResult, $result);
+      }
+    }
+    return $finalResult;
+  }
 }
 
 ?>
--- tools/genNGram.php
+++ tools/genNGram.php
@@ -14,11 +14,10 @@ $dbResult = db_execute("select * from Lexem", PDO::FETCH_ASSOC);
 $values = array();
 foreach ($dbResult as $cnt => $row) {
   $lexem = Model::factory('Lexem')->create($row);
-  $form = NGram::padWord($lexem->formNoAccent);
-  $len = mb_strlen($form);
+  $ngrams = NGram::split($lexem->formNoAccent);
 
-  for ($i = 0; $i < $len - NGram::$NGRAM_SIZE + 1; $i++) {
-    $values[] = array(addslashes(mb_substr($form, $i, NGram::$NGRAM_SIZE)), $i, $lexem->id);
+  foreach ($ngrams as $i => $ngram) {
+    $values[] = array($ngram, $i, $lexem->id);
   }
   if (count($values) >= INSERT_SIZE) {
     // Assemble low-level MySQL query. Idiorm inserts records one by one, which is many times slower.
@@ -27,7 +26,7 @@ foreach ($dbResult as $cnt => $row) {
       if ($i) {
 	$query .= ',';
       }
-      $query .= sprintf("('%s', %d, %d)", $set[0], $set[1], $set[2]);
+      $query .= sprintf("('%s', %d, %d)", addslashes($set[0]), $set[1], $set[2]);
     }
     db_execute($query);
     $values = array();

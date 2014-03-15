d846d1f19234ab2ce2e7d86f37a0c82415654260
grigoroiualex <grigoroiualexandru@gmail.com>
2014-03-15 12:49:20 +0200

    Cautare cuvinte test#1

--- wwwbase/ajax/scramble.php
+++ wwwbase/ajax/scramble.php
@@ -2,37 +2,49 @@
 require_once("../../phplib/util.php");
 setlocale(LC_ALL, "ro_RO.utf8");
 
-define('short', 5);
+$option = util_getRequestParameter('option');
+
+define('short', 6);
 define('medium', 8);
-define('long', 15);
-define('verylong', 30);
+define('long', 10);
+define('verylong', 20);
 
 switch ($option) {
 case 4:
+  $minLength = long;
   $maxLength = verylong;
   break;
 case 3:
   $maxLength = long;
+  $minLength = medium;
   break;
 case 2:
   $maxLength = medium;
+  $minLength = short;
   break;
 case 1:
   $maxLength = short;
+  $minLength = short;
   break;
 default :
-  $maxLength = easyLength;
+  $maxLength = short;
+  $minLength = short;
 }
 
 	$indexWords = Model::factory('Lexem')
-			->where_raw('charLength =>5')
-			->where_raw('charLength =<'.$maxLength)
+			->where_gte('charLength', $minLength)
+      ->where_lte('charLength', $maxLength)
 			->count();
 			
 	$lexem = Model::factory('Lexem')
-    ->where_raw('char_length(formUtf8General) >= 5')
-    ->where_raw('char_length(formUtf8General) <= '.$maxLength)
+    ->where_gte('charLength', $minLength)
+    ->where_lte('charLength', $maxLength)
     ->offset(rand(0, $indexWords - 1))
     ->find_one();
-}
+
+echo $lexem->formUtf8General;
+echo $indexWords;
+$result = array('noWords' => $indexWords, 'randomWord' => $lexem);
+echo json_encode($lexem);
+
 ?>

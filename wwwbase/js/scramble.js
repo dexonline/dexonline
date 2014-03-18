461320515eac49f598cf1e812912e3a576750208
lauraflu <flueratorulaura@gmail.com>
2014-03-18 14:57:51 +0200

    Not working, to be checked

--- wwwbase/js/scramble.js
+++ wwwbase/js/scramble.js
@@ -1,11 +1,13 @@
+var button = document.getElementsByClassName("testButton");
+
 $(document).ready(function() {
-	$(":button").click(function() {
+	button.addEventListener("click", function() {
 		var obj;
 		obj.difficulty = $("button.btn").attr("value");
 		$.ajax({
 			type: "POST",
 			url: "../ajax/scramble.php",
-			data: obj,
+			data: { difficulty : obj.difficulty },
 			}).done(function(response) {
 				alert("Merge!");
 				var word = $.parseJSON(response);

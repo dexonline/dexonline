726186cf414f42e00ab45a0846044b94de004005
Cătălin Frâncu <cata@francu.com>
2014-03-23 11:05:48 -0700

    Add Nginx config file

--- tools/git-hooks/post-merge.sh
+++ tools/git-hooks/post-merge.sh
@@ -13,7 +13,7 @@ changed_files="$(git diff-tree -r --name-only --no-commit-id HEAD@{1} HEAD)"
 messages=()
 
 check_run dex.conf.sample "Please reconcile your dex.conf with dex.conf.sample"
-check_run wwwbase/.htaccess.sample "Please reconcile your wwwbase/.htaccess with wwwbase/.htaccess.sample"
+check_run wwwbase/.htaccess.sample "Please reconcile your wwwbase/.htaccess with wwwbase/.htaccess.sample and/or Nginx"
 check_run tools/setup.sh "Please rerun tools/setup.sh on your client"
 check_run patches/ 'The database schema has changed. Please run "php tools/migration.php"'
 

1955ce5a6fda5b779ea5f7487ea6c9fbcd2fda8a
AdamBlade <bog9112@gmail.com>
2014-03-25 18:15:57 +0200

    Keyevents,more bugs,not working

--- wwwbase/js/scramble.js
+++ wwwbase/js/scramble.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 	$('.difficultyButton').on("click", function() {
 		// this e pentru a prelua valoarea butonului tocmai apasat, si nu a unuia oarecare
 		var difficulty = $(this).attr("value");
@@ -12,7 +13,7 @@ $(document).ready(function() {
 				$('#result').html(word.randomWord);
 				$('#noWords').html(word.noWords);
 				var letterArray = new Array();
-			    letterArray = word.charArray;
+			    letterArray = word.charArray
 			   	drawLetters(letterArray);
 		})
 		.fail(function() {
@@ -35,25 +36,51 @@ $(document).ready(function() {
 			console.log("Nu merge");
 		});
 	});
-	
+// printeaza literele cuvantului random din baza de date
 function drawLetters(array){
-	$('canvas').removeLayers().delay(800);
+	var layers;
+	$('canvas').removeLayers();
 	for (var i=0; i<=array.length; i++) {
+
+		var posX = 100+(i*30);
+
 		$('canvas').drawText({
   			draggable: true,
   			fillStyle: '#9cf',
   			strokeStyle: '#25a',
   			strokeWidth: 2,
   			x: 320, y: -30,
-  			fontSize: 25,
+  			fontSize: 32.5,
   			fontFamily: 'Verdana, sans-serif',
-  			text: array[i]
+  			text: array[i].toUpperCase(),
+  			name: array[i]
 		})
 	.animateLayer(i, {			
-  			x: 100+(i*30), y: 390,
-});	
+  			x: posX, y: 390
+		})	
   	}
-  }
+  	layers = $('canvas').getLayers();
+  	console.log(layers);
+  	keylisten(layers,posX);
+}
+//asculta tot documentul pentru apasarea unei taste, daca tasta corespunde numelui layer-ului atunci se se muta pozitia pozitia acelui layer pe Y = 150.
+function keylisten(layers,posX){
+	$(document).keypress(function(e){
+		var test = String.fromCharCode(e);
+		console.log(test);
+	})
+	for(var i = 0; i<=layers.length;i++) {	
+		if(test == layers[i]) {
+		$('canvas').animateLayer(i,{
+		x:posX, y: 150
+		});
+		}
+	}
+}
+
+
+
+// END OF FILE  
 });
 
 

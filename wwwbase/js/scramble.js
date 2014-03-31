291437bb7db2bf0d4683d5a01042d1ebc56720fb
Alexandru Marian Grigoroiu <grigoroiualexandru@gmail.com>
2014-04-01 00:37:53 +0300

    Apăsarea unei taste produce deplasarea literei corespunzătoare.

--- wwwbase/js/scramble.js
+++ wwwbase/js/scramble.js
@@ -7,58 +7,82 @@ $(document).ready(function() {
       type: "POST",
       url: wwwRoot + "ajax/scramble.php",
       data: { difficulty : difficulty },
-      })
-      .done(function(response) {
-        var word = $.parseJSON(response);
-        $("#result").html(word.randomWord);
-        $("#noWords").html(word.noWords);
-        drawLetters(word.randomWord);
-        console.log(word.randomWord);
-        $(".searchWord").val("");
-      })
-      .fail(function() {
-        console.log("Nu merge");
-      });
+    })
+    .done(function(response) {
+      var word = $.parseJSON(response);
+      $("#result").html(word.randomWord);
+      $("#noWords").html(word.noWords);
+      drawLetters(word.randomWord);
+      console.log(word.randomWord);
+      $(".searchWord").val("");
+    })
+    .fail(function() {
+      console.log("Nu merge");
     });
-// test pentru cuvinte returnate prin json
-  $(".searchWord").keyup(function() {
+  });
+
+  // test pentru cuvinte returnate prin json
+  $(".searchWord").keyup(function(letter) {
     var searchWord = $(this).val();
-      $.ajax({
+    $.ajax({
       type:"POST",
       url: wwwRoot + "ajax/scramble.php",
       data: { searchWord : searchWord },
-      })
-      .done(function(response){
-        var result = $.parseJSON(response);
-        $("#ifFound").html(result.Found);
-      })
-      .fail(function() {
-        console.log("Nu merge");
-      });
+    })
+    .done(function(response){
+      var result = $.parseJSON(response);
+      $("#ifFound").html(result.Found);
+    })
+    .fail(function() {
+      console.log("Nu merge");
     });
+
+    // asculta tot documentul pentru apasarea unei taste, daca tasta corespunde numelui layer-ului 
+    // atunci se muta pozitia acelui layer pe Y = 150.
+    layers = $("canvas").getLayers();
+    var key;
+    key = letter.keyCode;
+    for(i = 0; i < layers.length; i += 2) {
+      if(String.fromCharCode(key) == layers[i].data.letter && !layers[i].data.selected) {
+        $("canvas").animateLayerGroup("boggle" + i / 2, {
+          y: 200
+        });
+        layers[i].data.selected = true;
+
+        return;
+      }
+    }
+  });
+  
   // printeaza literele cuvantului random din baza de date
-  function drawLetters(array){
+  function drawLetters(array) {
     $("canvas").removeLayers();
     for (var i = 0; i < array.length; i++) {
 
       var posX = 50 + ( i * 40 );
 
       $("canvas").drawRect({
-        draggable: true,
-        name: array[i],
+        layer: true,
+        // draggable: true,
+        name: "rect" + i,
         fillStyle: "black",
-        groups: [i],
-        dragGroups: [i],
+        groups: ["boggle" + i],
+        // dragGroups: ["boggle" + i],
         x: 320, y: -30,
         width: 35,
         height: 45,
-        cornerRadius: 4
+        cornerRadius: 4,
+        data: {
+          letter: array[i].toUpperCase(),
+          selected: false
+        }
       })
       .drawText({
-        draggable: true,
-        name: array[i],
-        groups: [i],
-        dragGroups: [i],
+        layer: true,
+        // draggable: true,
+        name: "letter" + i,
+        groups: ["boggle" + i],
+        // dragGroups: ["boggle" + i],
         fillStyle: "#9cf",
         strokeStyle: "#25a",
         strokeWidth: 2,
@@ -67,55 +91,18 @@ $(document).ready(function() {
         fontFamily: "Verdana, sans-serif",
         text: array[i].toUpperCase(),
       })
-      .animateLayerGroup(i, {     
+      .animateLayerGroup("boggle" + i, {     
         x: posX, y: 50
       });
     }
 
-    var layers = $("canvas").getLayers();
-     console.log(layers);
-    keyListen(layers);
+    /*var layers = $("canvas").getLayers();
+    console.log(layers);
+    keyListen(layers);*/
   }
-
-// asculta tot documentul pentru apasarea unei taste, daca tasta corespunde numelui layer-ului 
-// atunci se muta pozitia acelui layer pe Y = 150.
-
-
- /* function keylisten(layers){
-    $(".animate").on("click", function(){
-      console.log($(".animate").attr("value"));
-      console.log(layers);
-      for(var i = 0; i < layers.length; i++) {  
-        $("canvas").animateLayerGroup(i, {
-        y: 200
-        });
-      }
-    });
-  }
-*/
-
-  function keyListen(layers){
-    $(".searchWord").keyup(function(letter){
-      var key;
-      key = letter.keyCode;
-      for(i = 0; i < layers.length; i++) {
-        if( String.fromCharCode(key) == (layers[i].name).toUpperCase() || String.fromCharCode(key) == (layers[i].name).toLowerCase() ) {  
-          $("canvas").animateLayerGroup(i, {
-            y: 200
-          });
-        }
-        
-      }
-    });
-  }
-
 
   // goleste continutul input-ului dupa ce pagina este reincarcata
   $(window).load(function() {
     $(".searchWord").val("");
   }); 
-
-
-
-
 });

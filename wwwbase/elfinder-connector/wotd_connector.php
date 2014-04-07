c90cbfca4453a5a12634985ce90dfbc0983627a4
Cătălin Frâncu <cata@francu.com>
2014-04-07 14:59:47 +0300

    Link the elfinder WotD connector to the static (FTP) server.

--- wwwbase/elfinder-connector/wotd_connector.php
+++ wwwbase/elfinder-connector/wotd_connector.php
@@ -5,30 +5,10 @@ error_reporting(0); // Set E_ALL for debuging
 include_once __DIR__ . '/elFinderConnector.class.php';
 include_once __DIR__ . '/elFinder.class.php';
 include_once __DIR__ . '/elFinderVolumeDriver.class.php';
-include_once __DIR__ . '/elFinderVolumeLocalFileSystem.class.php';
+include_once __DIR__ . '/elFinderVolumeFTP.class.php';
 include_once __DIR__ . '/elFinderLogger.class.php';
 
-// Required for MySQL storage connector
-// include_once __DIR__ . '/elFinderVolumeMySQL.class.php';
-// Required for FTP connector support
-// include_once __DIR__ . '/elFinderVolumeFTP.class.php';
-
 include_once __DIR__ . '/../../phplib/util.php';
-
-/**
- * Simple function to demonstrate how to control file access using "accessControl" callback.
- * This method will disable accessing files/folders starting from '.' (dot)
- *
- * @param  string  $attr  attribute name (read|write|locked|hidden)
- * @param  string  $path  file path relative to volume root directory started with directory separator
- * @return bool|null
- **/
-
-function access($attr, $path, $data, $volume) {
-  return (strpos(basename($path), '.')) === 0       // if file/folder begins with '.' (dot)
-    ? !($attr == 'read' || $attr == 'write')    // set read+write to false, other (locked+hidden) set to true
-    :  null;                                    // else elFinder decide it itself
-}
 
 $myLogger = new elFinderSimpleLogger('../../log/wotdelflog');
 
@@ -39,17 +19,20 @@ $opts = array(
     'mkdir mkfile rename duplicate upload rm paste' => array($myLogger, 'log')),
   'roots' => array(
     array(
-      'driver'        => 'LocalFileSystem', // driver for accessing file system (REQUIRED)
-      'path'          => '../img/wotd/', // path to files (REQUIRED)
-      'URL'      => util_getFullServerUrl() . '/img/wotd/', // URL to files (REQUIRED)
-      'accessControl' => 'access', // disable and hide dot starting files (OPTIONAL)
-      'alias'      => 'Imagini cuvântul zilei', // display this instead of root directory name
-      'uploadAllow'  => array('image'), // mimetypes allowed to upload
-      'disabled'    => array('resize'), // list of not allowed commands
-      'imgLib'    => 'gd', // image manipulation library (imagick, mogrify, gd)
-      'tmbPath'    => '.tmb', // directory name for image thumbnails. Set to "" to avoid thumbnails generation
-      'dirMode'   => 0777,
-      'fileMode'  => 0666,
+      'driver'        => 'FTP',
+      'host'          => Config::get('static.host'),
+      'user'          => Config::get('static.user'),
+      'pass'          => Config::get('static.password'),
+      'path'          => Config::get('static.path') . 'img/wotd/',
+      'timeout'       => Config::get('static.timeout'),
+      'URL'           => Config::get('static.url') . 'img/wotd/',
+      'alias'         => 'Imagini cuvântul zilei', // display this instead of root directory name
+      'uploadAllow'   => array('image'), // mimetypes allowed to upload
+      'imgLib'        => 'gd',
+
+      // Thumbnails are still stored locally
+      'tmbPath'       => '../img/wotd/thumb',
+      'tmbURL'        => '../img/wotd/thumb',
     )
   )
 );
@@ -58,3 +41,4 @@ $opts = array(
 $connector = new elFinderConnector(new elFinder($opts));
 $connector->run();
 
+?>

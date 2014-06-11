3e119b2ca8076a7c1a606ccbfbc3d0b0fb26e2ef
Cătălin Frâncu <cata@francu.com>
2014-06-11 11:53:08 +0300

    Convert scrabble stuff to use LexemModels

--- tools/rebuildScrabbleForms.php
+++ tools/rebuildScrabbleForms.php
@@ -18,20 +18,7 @@ foreach (Config::getLocVersions() as $version) {
     LocVersion::changeDatabase($version->name);
 
     log_scriptLog('* running ginormous query');
-    $query = 'select I.formNoAccent ' .
-      'from InflectedForm I, LexemModel LM, Lexem L, Model M, ModelDescription MD, ModelType MT ' .
-      'where I.lexemModelId = LM.id ' .
-      'and LM.lexemId = L.id ' .
-      'and LM.modelType = MT.code ' .
-      'and MT.canonical = M.modelType ' .
-      'and LM.modelNumber = M.number ' .
-      'and M.id = MD.modelId ' .
-      'and MD.inflectionId = I.inflectionId ' .
-      'and MD.variant = I.variant ' .
-      'and MD.applOrder = 0 ' .
-      'and L.isLoc and MD.isLoc ' .
-      'and char_length(I.formNoAccent) between 2 and 15';
-    $dbResult = db_execute($query);
+    $dbResult = getRawForms($version->name);
 
     log_scriptLog('* creating raw file');
     $fileName = "/tmp/forme-{$version->name}-raw.txt";
@@ -65,5 +52,40 @@ foreach (Config::getLocVersions() as $version) {
 }
 
 log_scriptLog('rebuildScrabbleForms: ending');
+
+/***************************************************************************/
+
+function getRawForms($locVersion) {
+  if ($locVersion >= '6.0') {
+    // LOC 6.0 introduces LexemModels
+    $query = 'select I.formNoAccent ' .
+      'from InflectedForm I, LexemModel LM, Lexem L, Model M, ModelDescription MD, ModelType MT ' .
+      'where I.lexemModelId = LM.id ' .
+      'and LM.lexemId = L.id ' .
+      'and LM.modelType = MT.code ' .
+      'and MT.canonical = M.modelType ' .
+      'and LM.modelNumber = M.number ' .
+      'and M.id = MD.modelId ' .
+      'and MD.inflectionId = I.inflectionId ' .
+      'and MD.variant = I.variant ' .
+      'and MD.applOrder = 0 ' .
+      'and LM.isLoc and MD.isLoc ' .
+      'and char_length(I.formNoAccent) between 2 and 15';
+  } else {
+    $query = 'select I.formNoAccent ' .
+      'from InflectedForm I, Lexem L, Model M, ModelDescription MD, ModelType MT ' .
+      'where I.lexemId = L.id ' .
+      'and L.modelType = MT.code ' .
+      'and MT.canonical = M.modelType ' .
+      'and L.modelNumber = M.number ' .
+      'and M.id = MD.modelId ' .
+      'and MD.inflectionId = I.inflectionId ' .
+      'and MD.variant = I.variant ' .
+      'and MD.applOrder = 0 ' .
+      'and L.isLoc and MD.isLoc ' .
+      'and char_length(I.formNoAccent) between 2 and 15';
+  }
+  return db_execute($query);
+}
 
 ?>
--- wwwbase/scrabble-flexiune.php
+++ wwwbase/scrabble-flexiune.php
@@ -10,18 +10,10 @@ if ($locVersion && $form) {
   $form = StringUtil::cleanupQuery($form);
   SmartyWrap::assign('page_title', 'Verificare LOC: ' . $form);
 
-  $ifs = loadLoc($form);
-  $lexems = array();
-  $inflections = array();
-  foreach ($ifs as $if) {
-    $lexems[] = Lexem::get_by_id($if->lexemId);
-    $inflections[] = Inflection::get_by_id($if->inflectionId);
-  }
+  $data = loadLoc($form, $locVersion);
   SmartyWrap::assign('form', $form);
   SmartyWrap::assign('selectedLocVersion', $locVersion);
-  SmartyWrap::assign('ifs', $ifs);
-  SmartyWrap::assign('lexems', $lexems);
-  SmartyWrap::assign('inflections', $inflections);
+  SmartyWrap::assign('data', $data);
 } else {
   SmartyWrap::assign('selectedLocVersion', $locVersions[1]->name);
   SmartyWrap::assign('page_title', 'Căutare formă flexionară în LOC ' . $form);
@@ -31,15 +23,58 @@ setlocale(LC_ALL, "ro_RO.utf8");
 SmartyWrap::assign('locVersions', $locVersions);
 SmartyWrap::displayCommonPageWithSkin('scrabble-flexiune.ihtml');
 
+/***************************************************************************/
 
-function loadLoc($cuv) {
+function loadLoc($cuv, $locVersion) {
   $field = StringUtil::hasDiacritics($cuv) ? 'formNoAccent' : 'formUtf8General';
-  return Model::factory('InflectedForm')
-    ->raw_query("select distinct I.* from InflectedForm I, Lexem L, Model M, ModelDescription MD, ModelType MT " .
-                "where I.lexemId = L.id and L.modelType = MT.code and MT.canonical = M.modelType and L.modelNumber = M.number and M.id = MD.modelId " .
-                "and I.inflectionId = MD.inflectionId and I.variant = MD.variant and MD.applOrder = 0 " .
-                "and I.{$field} = '{$cuv}' and L.isLoc and MD.isLoc order by L.formNoAccent", null)
-    ->find_many();
+
+  if ($locVersion >= '6.0') {
+    // LOC 6.0 introduces LexemModels
+    return Model::factory('InflectedForm')
+      ->table_alias('I')
+      ->select('I.form', 'inflectedForm')
+      ->select('L.id', 'lexemId')
+      ->select('L.formNoAccent', 'lexemFormNoAccent')
+      ->select('L.form', 'lexemForm')
+      ->select('LM.modelType', 'modelType')
+      ->select('LM.modelNumber', 'modelNumber')
+      ->select('LM.restriction', 'restriction')
+      ->select('Infl.description', 'inflection')
+      ->join('LexemModel', 'I.lexemModelId = LM.id', 'LM')
+      ->join('Lexem', 'LM.lexemId = L.id', 'L')
+      ->join('ModelType', 'LM.modelType = MT.code', 'MT')
+      ->join('Model', 'MT.canonical = M.modelType and LM.modelNumber = M.number', 'M')
+      ->join('ModelDescription', 'M.id = MD.modelId and I.variant = MD.variant and I.inflectionId = MD.inflectionId', 'MD')
+      ->join('Inflection', 'I.inflectionId = Infl.id', 'Infl')
+      ->where('MD.applOrder', 0)
+      ->where("I.{$field}", $cuv)
+      ->where('LM.isLoc', 1)
+      ->where('MD.isLoc', 1)
+      ->order_by_asc('LM.lexemId')
+      ->find_array();
+  } else {
+    return Model::factory('InflectedForm')
+      ->table_alias('I')
+      ->select('I.form', 'inflectedForm')
+      ->select('L.id', 'lexemId')
+      ->select('L.formNoAccent', 'lexemFormNoAccent')
+      ->select('L.form', 'lexemForm')
+      ->select('L.modelType', 'modelType')
+      ->select('L.modelNumber', 'modelNumber')
+      ->select('L.restriction', 'restriction')
+      ->select('Infl.description', 'inflection')
+      ->join('Lexem', 'I.lexemId = L.id', 'L')
+      ->join('ModelType', 'L.modelType = MT.code', 'MT')
+      ->join('Model', 'MT.canonical = M.modelType and L.modelNumber = M.number', 'M')
+      ->join('ModelDescription', 'M.id = MD.modelId and I.variant = MD.variant and I.inflectionId = MD.inflectionId', 'MD')
+      ->join('Inflection', 'I.inflectionId = Infl.id', 'Infl')
+      ->where('MD.applOrder', 0)
+      ->where("I.{$field}", $cuv)
+      ->where('L.isLoc', 1)
+      ->where('MD.isLoc', 1)
+      ->order_by_asc('L.id')
+      ->find_array();
+  }
 }
 
 ?>

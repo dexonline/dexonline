6c6bb3a35a41a80e7584610e0c2f3d878291afa3
Cătălin Frâncu <cata@francu.com>
2014-07-08 16:30:51 +0300

    Replace anchors with spans for meaning mentions

--- phplib/AdminStringUtil.php
+++ phplib/AdminStringUtil.php
@@ -349,7 +349,7 @@ class AdminStringUtil {
   }
 
   static function convertMeaningMentionsToHtml($s) {
-    return preg_replace('/\b([a-zăâîșț]+)\[([0-9]+)\]/i', '<a class="mention" title="$2" href="#">$1</a>', $s);
+    return preg_replace('/\b([a-zăâîșț]+)\[([0-9]+)\]/i', '<span class="mention" title="$2">$1</span>', $s);
   }
 
   /**

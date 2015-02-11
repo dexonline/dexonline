0f3eb9fe8de7dda763dcb602a31ba0c67e900a69
Radu Borza <radu_borza@yahoo.it>
2015-02-11 15:42:47 +0200

    add syntx check

---
+++ test/SyntaxTest.php
@@ -0,0 +1,27 @@
+<?php
+
+class SyntaxTest extends PHPUnit_Framework_TestCase {
+    public function testPhpSyntax()
+    {
+        $php = PHP_BINARY;
+        $folders = array('wwwbase', 'phplib', 'tools');
+        $errors = array();
+
+        foreach ($folders as $dir) {
+            $directory = dirname(__DIR__) . "/$dir";
+            $dirIt = new RecursiveDirectoryIterator($directory);
+            $itIt = new RecursiveIteratorIterator($dirIt);
+            $files = new RegexIterator($itIt, '/\.php$/');
+
+
+            foreach ($files as $file) {
+                $summary = exec("$php -l $file", $output, $statusCode);
+                if ($statusCode !== 0) {
+                    $errors[] = $summary;
+                }
+            }
+        }
+
+        $this->assertCount(0, $errors, implode("\n", $errors));
+    }
+}

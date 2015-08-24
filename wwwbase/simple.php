e9f789c794b18d39659b6ad81f46d7842de01f30
Cătălin Frâncu <cata@francu.com>
2015-08-24 13:19:14 +0300

    return 404 for error pages

--- wwwbase/simple.php
+++ wwwbase/simple.php
@@ -3,7 +3,10 @@ require_once("../phplib/util.php");
 $p = util_getRequestParameter('p');
 
 switch($p) {
-  case '404': SmartyWrap::assign('page_title', 'Pagină inexistentă'); break;
+  case '404':
+    SmartyWrap::assign('page_title', 'Pagină inexistentă');
+    http_response_code(404);
+    break;
   case 'contact': SmartyWrap::assign('page_title', 'Contact'); break;
   case 'links': SmartyWrap::assign('page_title', 'Legături'); break;
   case 'ads': SmartyWrap::assign('page_title', 'Publicitate pe DEX online'); break;

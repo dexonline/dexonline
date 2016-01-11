86e6854d11f047b0d806ca89db53a1f3ad764441
Cătălin Frâncu <cata@francu.com>
2016-01-11 12:52:55 +0200

    Switch from positive restrictions to negative restrictions in the ConstraintMap table. This will simplify vocative restrictions. Addresses #423.

--- phplib/models/ConstraintMap.php
+++ phplib/models/ConstraintMap.php
@@ -7,11 +7,11 @@ class ConstraintMap extends BaseObject {
    * Given a restriction like 'PT', and an inflection, returns true iff the inflection ID is valid under all the restrictions.
    */
   public static function validInflection($inflId, $restr) {
-    if (!$restr) {
-      return true;
-    }
-    $numAllowed = Model::factory('ConstraintMap')->where_raw("locate(code, '$restr') > 0")->where('inflectionId', $inflId)->count();
-    return ($numAllowed == mb_strlen($restr));
+    $count = Model::factory('ConstraintMap')
+           ->where_raw("locate(code, '$restr') > 0")
+           ->where('inflectionId', $inflId)
+           ->count();
+    return !$count;
   }
 }
 

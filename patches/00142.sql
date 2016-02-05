d52c4ec00735389537b895cc0901e14639000aa6
Cătălin Frâncu <cata@francu.com>
2016-02-05 14:36:16 +0200

    Change wotd image file structure

---
+++ patches/00142.sql
@@ -0,0 +1,5 @@
+update WordOfTheDay set image = replace(image, '2012-', '2012/');
+update WordOfTheDay set image = replace(image, '2013-', '2013/');
+update WordOfTheDay set image = replace(image, '2014-', '2014/');
+update WordOfTheDay set image = replace(image, '2015-', '2015/');
+update WordOfTheDay set image = replace(image, '2016-', '2016/');

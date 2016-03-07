e42b86ebbca18ed1f75df80f1b5f5e87796b8eb7
Cătălin Frâncu <cata@francu.com>
2016-03-07 17:26:45 +0200

    Show old WotD descriptions. Fixes #438. Show WotD descriptions as tooltips in the right sidebox.

--- wwwbase/wotd.php
+++ wwwbase/wotd.php
@@ -2,7 +2,6 @@
 
 define('ONE_DAY_IN_SECS',86400);
 define('WOTD_BIG_BANG', '2011-05-01');
-define('WOTD_REASON_BIG_BANG', '2013-09-01');
 define('WOTD_REASON_DISPLAY_DELAY', 2);
 
 require_once("../phplib/util.php");
@@ -74,8 +73,8 @@ if ($wotd) {
   $reason = $wotd->description;
   if (
     util_isModerator(PRIV_ADMIN) || 
-    ($mysqlDate >= WOTD_REASON_BIG_BANG && $date && strtotime($date) < time() - WOTD_REASON_DISPLAY_DELAY * ONE_DAY_IN_SECS) 
-    ) {
+    ($date && strtotime($date) < time() - WOTD_REASON_DISPLAY_DELAY * ONE_DAY_IN_SECS) 
+  ) {
     SmartyWrap::assign('reason', $reason);
   }
 }

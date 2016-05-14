8577ec021f95596aa33749767f123c448d2922f3
David Batranu <dbatranu@gmail.com>
2016-05-15 00:10:08 +0300

    [refs #412] (WIP) responsive-layout + homepage
    * adding `responsive.css` for customizations
    * adding elements from `zepu/index.tpl` into `responsive/index.tpl` and
      structuring them into rows/columns

--- phplib/SmartyWrap.php
+++ phplib/SmartyWrap.php
@@ -152,20 +152,21 @@ class SmartyWrap {
         case 'responsive':
           self::$cssFiles[10] = 'bootstrap/css/bootstrap.min.css';
           self::$cssFiles[11] = 'bootstrap/css/bootstrap-theme.min.css';
+          self::$cssFiles[12] = 'responsive.css';
           break;
-        case 'zepu':                self::$cssFiles[12] = 'zepu.css'; break;
-        case 'polar':               self::$cssFiles[13] = 'polar.css'; break;
-        case 'mobile':              self::$cssFiles[14] = 'mobile.css'; break;
-        case 'flex':                self::$cssFiles[15] = 'flex.css'; break;
-        case 'paradigm':            self::$cssFiles[16] = 'paradigm.css'; break;
-        case 'jcrop':               self::$cssFiles[17] = 'jcrop/jquery.Jcrop.min.css'; break;
-        case 'select2':             self::$cssFiles[18] = 'select2/select2.min.css'; break;
+        case 'zepu':                self::$cssFiles[13] = 'zepu.css'; break;
+        case 'polar':               self::$cssFiles[14] = 'polar.css'; break;
+        case 'mobile':              self::$cssFiles[15] = 'mobile.css'; break;
+        case 'flex':                self::$cssFiles[16] = 'flex.css'; break;
+        case 'paradigm':            self::$cssFiles[17] = 'paradigm.css'; break;
+        case 'jcrop':               self::$cssFiles[18] = 'jcrop/jquery.Jcrop.min.css'; break;
+        case 'select2':             self::$cssFiles[19] = 'select2/select2.min.css'; break;
         case 'gallery':
-          self::$cssFiles[19] = 'colorbox/colorbox.css';
-          self::$cssFiles[20] = 'visualDict.css';
+          self::$cssFiles[20] = 'colorbox/colorbox.css';
+          self::$cssFiles[21] = 'visualDict.css';
           break;
-        case 'textComplete':        self::$cssFiles[21] = 'jquery.textcomplete.css'; break;
-        case 'flash':               self::$cssFiles[22] = 'flash.css'; break;
+        case 'textComplete':        self::$cssFiles[22] = 'jquery.textcomplete.css'; break;
+        case 'flash':               self::$cssFiles[23] = 'flash.css'; break;
         default:
           FlashMessage::add("Cannot load CSS file {$id}");
           util_redirect(util_getWwwRoot());

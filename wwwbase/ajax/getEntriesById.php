67bfee2138f46bd771af3b3859a92c5cd0a1f8fd
Cătălin Frâncu <cata@francu.com>
2016-05-30 18:47:42 +0300

    Show the entryId field on the lexem edit page. Addresses #66.

---
+++ wwwbase/ajax/getEntriesById.php
@@ -0,0 +1,29 @@
+<?php
+require_once("../../phplib/util.php");
+
+// Takes a JSON-encoded list of ids
+
+$jsonIds = util_getRequestParameter('q');
+$ids = json_decode($jsonIds);
+$data = [];
+
+foreach ($ids as $id) {
+  $e = Entry::get_by_id($id);
+
+  if ($e) {
+    $data[] = [
+      'id' => $id,
+      'text' => $e->description,
+    ];
+  } else {
+    $data[] = [
+      'id' => 0,
+      'text' => '',
+    ];
+  }
+}
+
+header('Content-Type: application/json');
+print json_encode($data);
+
+?>

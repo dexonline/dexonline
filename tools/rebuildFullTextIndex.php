0fff06c423979ea67a32655a738d9f31fb628f33
Cătălin Frâncu <cata@francu.com>
2016-06-02 12:48:06 +0300

    convert code related to the full text index. Addresses #66.

--- tools/rebuildFullTextIndex.php
+++ tools/rebuildFullTextIndex.php
@@ -4,6 +4,8 @@ ini_set('max_execution_time', '3600');
 ini_set('memory_limit', '256M');
 assert_options(ASSERT_BAIL, 1);
 
+define('BATCH_SIZE', 10000);
+
 Log::notice('started');
 if (!Lock::acquire(LOCK_FULL_TEXT_INDEX)) {
   OS::errorAndExit('Lock already exists!');
@@ -15,52 +17,57 @@ db_execute('truncate table FullTextIndex');
 
 $stopWordForms = array_flip(db_getArray(
   'select distinct i.formNoAccent ' .
-  'from Lexem l, LexemModel lm, InflectedForm i ' .
-  'where l.id = lm.lexemId ' .
-  'and lm.id = i.lexemModelId ' .
+  'from Lexem l, InflectedForm i ' .
+  'where l.id = i.lexemId ' .
   'and l.stopWord'));
 
-$ifMap = array();
-$dbResult = db_execute('select id, internalRep from Definition where status = 0');
-$numDefs = $dbResult->rowCount();
-$defsSeen = 0;
+$ifMap = [];
+$offset = 0;
 $indexSize = 0;
 $fileName = tempnam(Config::get('global.tempDir'), 'index_');
 $handle = fopen($fileName, 'w');
 Log::info("Writing index to file $fileName.");
 DebugInfo::disable();
 
-foreach ($dbResult as $dbRow) {
-  $words = extractWords($dbRow[1]);
+do {
+  $defs = Model::factory('Definition')
+        ->select('id')
+        ->select('internalRep')
+        ->where('status', Definition::ST_ACTIVE)
+        ->limit(BATCH_SIZE)
+        ->offset($offset)
+        ->find_array();
 
-  foreach ($words as $position => $word) {
-    if (!isset($stopWordForms[$word])) {
-      if (!array_key_exists($word, $ifMap)) {
-        cacheWordForm($word);
-      }
-      if (array_key_exists($word, $ifMap)) {
-        $lexemList = preg_split('/,/', $ifMap[$word]);
-        for ($i = 0; $i < count($lexemList); $i += 2) {
-          fwrite($handle, $lexemList[$i] . "\t" . $lexemList[$i + 1] . "\t" . $dbRow[0] . "\t" . $position . "\n");
-          $indexSize++;
+  foreach ($defs as $d) {
+    $words = extractWords($d['internalRep']);
+
+    foreach ($words as $position => $word) {
+      if (!isset($stopWordForms[$word])) {
+        if (!array_key_exists($word, $ifMap)) {
+          cacheWordForm($word);
         }
-      } else {
-        // print "Not found: $word\n";
+        if (array_key_exists($word, $ifMap)) {
+          $lexemList = preg_split('/,/', $ifMap[$word]);
+          for ($i = 0; $i < count($lexemList); $i += 2) {
+            fwrite($handle, $lexemList[$i] . "\t" . $lexemList[$i + 1] . "\t" . $d['id'] . "\t" . $position . "\n");
+            $indexSize++;
+          }
+        } else {
+          // print "Not found: $word\n";
+        }
       }
     }
   }
 
-  if (++$defsSeen % 10000 == 0) {
-    $runTime = DebugInfo::getRunningTimeInMillis() / 1000;
-    $speed = round($defsSeen / $runTime);
-    Log::info("$defsSeen of $numDefs definitions indexed ($speed defs/sec). " .
-                  "Word map has " . count($ifMap) . " entries. " .
-                  "Memory used: " . round(memory_get_usage() / 1048576, 1) . " MB.");
-  }
-}
+  $offset += BATCH_SIZE;
+  $runTime = DebugInfo::getRunningTimeInMillis() / 1000;
+  $speed = round($offset / $runTime);
+  Log::info("$offset definitions indexed ($speed defs/sec). " .
+            "Word map has " . count($ifMap) . " entries. " .
+            "Memory used: " . round(memory_get_usage() / 1048576, 1) . " MB.");
+} while (count($defs) == BATCH_SIZE);
 
 fclose($handle);
-Log::info("$defsSeen of $numDefs definitions indexed.");
 Log::info("Index size: $indexSize entries.");
 
 OS::executeAndAssert("chmod 666 $fileName");
@@ -105,10 +112,10 @@ function extractWords($text) {
 // Look up all lexems that generate this word form and that are not stop words
 function cacheWordForm($word) {
   global $ifMap;
-  $dbResult = db_execute("select lexemModelId, inflectionId from InflectedForm where formNoAccent = '{$word}'");
+  $dbResult = db_execute("select lexemId, inflectionId from InflectedForm where formNoAccent = '{$word}'");
   $value = '';
   foreach ($dbResult as $dbRow) {
-    $value .= ',' . $dbRow['lexemModelId'] . ',' . $dbRow['inflectionId'];
+    $value .= ',' . $dbRow['lexemId'] . ',' . $dbRow['inflectionId'];
   }
   if ($value) {
     $ifMap[$word] = substr($value, 1);

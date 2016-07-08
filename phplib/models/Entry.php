db5ec647667d595c39a1b502b53504a64e2658c2
Cătălin Frâncu <cata@francu.com>
2016-07-08 18:38:44 +0300

    Move structStatus and structuristId from Lexem to Entry. Addresses #66.

--- phplib/models/Entry.php
+++ phplib/models/Entry.php
@@ -6,9 +6,22 @@ class Entry extends BaseObject implements DatedObject {
   private $lexems = null;
   private $trees = null;
 
+  const STRUCT_STATUS_NEW = 1;
+  const STRUCT_STATUS_IN_PROGRESS = 2;
+  const STRUCT_STATUS_UNDER_REVIEW = 3;
+  const STRUCT_STATUS_DONE = 4;
+
+  public static $STRUCT_STATUS_NAMES = [
+    self::STRUCT_STATUS_NEW => 'neîncepută',
+    self::STRUCT_STATUS_IN_PROGRESS => 'în lucru',
+    self::STRUCT_STATUS_UNDER_REVIEW => 'așteaptă moderarea',
+    self::STRUCT_STATUS_DONE => 'terminată',
+  ];
+
   static function createAndSave($description) {
     $e = Model::factory('Entry')->create();
     $e->description = $description;
+    $e->structStatus = self::STRUCT_STATUS_NEW;
     $e->save();
     return $e;
   }
@@ -69,12 +82,43 @@ class Entry extends BaseObject implements DatedObject {
 
   /**
    * Validates an entry for correctness. Returns an array of { field => array of errors }.
+   * $original: the original, unmodified entry
    **/
-  function validate() {
+  function validate($original) {
     $errors = [];
 
     if (!mb_strlen($this->description)) {
-      $errors['description'][] = _('Descrierea nu poate fi vidă.');
+      $errors['description'][] = 'Descrierea nu poate fi vidă.';
+    }
+
+    if (($this->structStatus == Entry::STRUCT_STATUS_DONE) &&
+        ($original->structStatus != Entry::STRUCT_STATUS_DONE) &&
+        !util_isModerator(PRIV_EDIT)) {
+      $errors['structStatus'][] = 'Doar moderatorii pot marca structurarea drept terminată. ' .
+                                'Vă rugăm să folosiți valoarea „așteaptă moderarea”.';
+    }
+
+    if ($this->structuristId != $original->structuristId) {
+      if (util_isModerator(PRIV_ADMIN)) {
+        // Admins can modify this field
+      } else if (($original->structuristId == session_getUserId()) &&
+                 !$this->structuristId) {
+        // Structurists can remove themselves
+      } else if (!$original->structuristId &&
+                 ($this->structuristId == session_getUserId()) &&
+                 ($original->structStatus == Entry::STRUCT_STATUS_NEW) &&
+                 ($this->structStatus == Entry::STRUCT_STATUS_IN_PROGRESS)) {
+        // The system silently assigns structurists when they start the process
+      } else if (!$original->structuristId &&
+                 ($this->structuristId == session_getUserId()) &&
+                 ($original->structStatus == Entry::STRUCT_STATUS_IN_PROGRESS) &&
+                 ($this->structStatus == Entry::STRUCT_STATUS_IN_PROGRESS)) {
+        // Structurists can claim orphan entries
+      } else {
+        $errors['structuristId'] = 'Nu puteți modifica structuristul, dar puteți (1) revendica ' .
+                                 'o intrare în lucru fără structurist sau (2) renunța la ' .
+                                 'o intrare dacă vi se pare prea greu de structurat.';
+      }
     }
 
     return $errors;

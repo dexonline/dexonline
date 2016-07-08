12da0616494faec3bd0a76013aff2cf1c9d66e69
Cătălin Frâncu <cata@francu.com>
2016-07-08 16:42:48 +0300

    Fix meaning tooltips. Meaning cleanup. Addresses #66.

--- phplib/models/MeaningTag.php
+++ phplib/models/MeaningTag.php
@@ -3,12 +3,6 @@
 class MeaningTag extends BaseObject implements DatedObject {
   public static $_table = 'MeaningTag';
 
-  public static function deleteByMeaningId($meaningId) {
-    $mts = self::get_all_by_meaningId($meaningId);
-    foreach ($mts as $mt) {
-      $mt->delete();
-    }
-  }
 }
 
 ?>

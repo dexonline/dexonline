4d112cf44cd595d2b40509782fa4780f819c949f
Cătălin Frâncu <cata@francu.com>
2016-08-29 14:42:07 +0300

    Fix hangman difficulty buttons. Some cosmetic changes.

--- wwwbase/js/autoload/hangman.js
+++ wwwbase/js/autoload/hangman.js
@@ -4,34 +4,36 @@ $(function() {
   var lettersLeft = word.length;
 
   function init() {
-    $('.letterButtons').click(function() { hangman_letterPressed($(this)); });
-    $('#hintButton').click(function() { hangman_hint(); });
-    $('.newGame').click(function() { hangman_newGame($(this).attr('name').split('_', 2)[1]); });
+    $('.letterButtons').click(function() { letterPressed($(this)); });
+    $('#hintButton').click(function() { hint(); });
+    $('.newGameControls button').click(function() {
+        newGame($(this).attr('data-level'));
+    });
     revealLetters(difficulty);
     $('.letters').focus();
     
     document.addEventListener('keypress', function(event) {
       var char = String.fromCharCode(event.charCode).toUpperCase();
       if(char == ' ') {
-        hangman_hint();
+        hint();
       } else if (char >= '1' && char <= '4') {
-        hangman_newGame(char);
+        newGame(char);
       } else if (char.match(/[A-ZĂÎȘȚÂ]/g)) {
         //creating letterButton & matching key only with Hangman relevant chars
         var field = $('.letterButtons[value="' + char + '"]');
-        hangman_letterPressed(field);
+        letterPressed(field);
       }
     });
   }
 
-  function hangman_updateLives() {
+  function updateLives() {
     $('.hangmanPic').css('background-position', '-' + (lives * 250 + 10) + 'px -10px');
     $("#livesLeft").fadeOut(function() {
       $(this).text(lives).fadeIn();
     });
   }
 
-  function hangman_gameOver() {
+  function gameOver() {
     if (!lives || !lettersLeft) {
       jQuery.noticeAdd({
         text: lives ? 'Felicitări, ai câștigat!' : 'Ne pare rău, ai pierdut.',
@@ -49,7 +51,7 @@ $(function() {
   }
 
   /* Returns true if any letters were uncovered */
-  function hangman_updateLetters(letter) {
+  function updateLetters(letter) {
     var ok = 0;
     for (i = 0; i < word.length; i++) {
       if (letter == word.charAt(i)) {
@@ -61,23 +63,23 @@ $(function() {
     return ok;
   }
 
-  function hangman_letterPressed(field) {
+  function letterPressed(field) {
     if(field.is(':disabled')) { //because of keyboard support one can press already pressed buttons
       return;
     }
-    var ok = hangman_updateLetters(field.val());
+    var ok = updateLetters(field.val());
     if (ok) {
       field.addClass('buttonGuessed');
     } else {
       lives--;
-      hangman_updateLives();
+      updateLives();
       field.addClass('buttonMissed');
     }
     field.attr('disabled', 'disabled');
-    hangman_gameOver();
+    gameOver();
   }
   
-  function hangman_hint() {
+  function hint() {
     // Grab a random letter that wasn't revealed yet
     letters = $('.letters');
 
@@ -89,33 +91,32 @@ $(function() {
     // Simulate a press of the corresponding button
     var button = $('.letterButtons[value="' + word.charAt(i) + '"]');
     button.addClass('buttonHinted');
-    hangman_updateLetters(word.charAt(i));
+    updateLetters(word.charAt(i));
     lives = (lives >= 2) ? lives - 2 : 0;
-    hangman_updateLives();
-    hangman_gameOver();
+    updateLives();
+    gameOver();
   }
 
-  function hangman_newGame(difficulty) {
+  function newGame(difficulty) {
     window.location = "spanzuratoarea?d=" + difficulty;
   }
 
   function revealLetters(difficulty) {
     //showing the '-' character
-    hangman_updateLetters('-');
+    updateLetters('-');
     if (difficulty > 2) {
       return;
     }
     firstLetter = word.charAt(0);
     lastLetter = word.charAt(word.length - 1);
     var field = $('.letterButtons[value="' + firstLetter + '"]');
-    hangman_letterPressed(field);
+    letterPressed(field);
 
     if (lastLetter != firstLetter) {
       field = $('.letterButtons[value="' + lastLetter + '"]');
-      hangman_letterPressed(field);
+      letterPressed(field);
     }
   }
-
 
   init();
 

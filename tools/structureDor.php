ce4f5531bf6dceae66e00835df8767f3b55470ea
Cătălin Frâncu <cata@francu.com>
2016-10-05 17:59:32 +0300

    DOR parser improvements. Addresses #517.

--- tools/structureDor.php
+++ tools/structureDor.php
@@ -3,25 +3,30 @@
 /**
  * Parse definitions from DOR, add LexemSources and mark them as structured
  * when possible.
+ *
+ * TODO
+ * - split inflected forms containing slashes
+ * - ignore the "simb." inflection (chemical symbols)
  **/
 
 require_once __DIR__ . '/../phplib/util.php';
 
 $DOR_SOURCE_ID = 38;
+$URL = 'https://dexonline.ro/admin/definitionEdit.php?definitionId='; // for edit links
 
 // regex parts
 $PART_PRON = '( \[.*(pr\.|cit\.).*\])';
-$PART_HYPH = '( \(sil\. \$[-a-zăâîșț]+\$\))';
+$PART_HYPH = '( \(sil\. [^)]+\))';
 
 $REGEX_WORD = "/^@([^@]+)@\s+/";
-$REGEX_POS = "/^([a-z. ]+){$PART_PRON}?{$PART_HYPH}?([,;] |$)/";
-$REGEX_INFL = "/^([-a-z. ]+) [$]([-a-zăâîșțáéíóú]+)[$]{$PART_HYPH}?([,;] |$)/";
+$REGEX_POS = "/^([a-z.\/ ]+){$PART_PRON}?{$PART_HYPH}?([,;] |$)/";
+$REGEX_INFL = "/^([-a-zăâîșț1-3. ]+) [$]([-a-zăâîșțáéíóúắấ \\/]+)[$]{$PART_HYPH}?([,;] |$)/";
 
 $defs = Model::factory('Definition')
       ->select('id')
       ->where('sourceId', $DOR_SOURCE_ID)
       ->where('structured', 0)
-      // ->where_in('lexicon', ['aerarium'])
+      //->where_in('lexicon', ['aerisi'])
       ->where_in('status', [Definition::ST_ACTIVE, Definition::ST_HIDDEN])
       ->find_many();
 
@@ -68,12 +73,17 @@ foreach ($defs as $i => $defId) {
       ];
     }
 
+    if ($s) {
+      var_dump($s);
+      throw new Exception('Cannot parse inflection list');
+    }
+
     // printf("Base form: [{$baseForm}]\n");
     // print_r($posList);
     // print_r($inflList);
 
   } catch (Exception $e) {
-    Log::warning('%s: %s', $e->getMessage(), $d->internalRep);
+    Log::warning('%s: %s [%s%d]', $e->getMessage(), $d->internalRep, $URL, $d->id);
     exit;
   }
   

325ecea8870e0b917562ec9b81f9c3303f32f46b
Radu Borza <horia.borza@payu.ro>
2016-11-15 18:22:54 +0200

    remove unneeded srcset

--- tools/syncWikiArticles.php
+++ tools/syncWikiArticles.php
@@ -158,6 +158,7 @@ function parse($text) {
   //Use media files from wiki
   $html = preg_replace('/src="\/(.*)\.(jpg|png|gif)"/', 'src="' . WIKI_BASE . '/$1.$2"', $html);
   $html = preg_replace('/href="\/(.*)\.(jpg|png|gif)"/', 'href="' . WIKI_BASE . '/$1.$2"', $html);
+  $html = preg_replace('/srcset="(.*)"/', '', $html);
 
   // Convert links to other articles, even if they are not under [[Categorie:Sincronizare]]
   $html = str_replace('href="/wiki/', 'href="/articol/', $html);

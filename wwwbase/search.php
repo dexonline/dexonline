cd65ae66684b9390238e40911dc9f05c69259d58
Cătălin Frâncu <cata@francu.com>
2016-11-21 17:16:20 +0200

    Fix sorting in single-word full-text searches.

--- wwwbase/search.php
+++ wwwbase/search.php
@@ -70,6 +70,14 @@ if ($text) {
     foreach ($defIds as $id) {
       $definitions[] = Definition::get_by_id($id);
     }
+
+    // For single-word queries, just order the definitions by lexicon.
+    if (count($words) - count($stopWords) == 1) {
+      usort($definitions, function($a, $b) {
+        return strcoll($a->lexicon, $b->lexicon) > 0;
+      });
+    }
+
     if (!count($defIds)) {
       FlashMessage::add('Nicio definiție nu conține toate cuvintele căutate.');
     }

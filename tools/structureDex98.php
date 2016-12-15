04140a11cc47f7887431d09e3c2f7dccd74554dc
Cătălin Frâncu <cata@francu.com>
2016-12-15 19:10:26 +0200

    More work on etymologies. Addresses #538.

--- tools/structureDex98.php
+++ tools/structureDex98.php
@@ -11,7 +11,7 @@ ini_set('memory_limit', '1024M');
 define('SOURCE_ID', 1);
 define('MY_USER_ID', 1);
 define('BATCH_SIZE', 10000);
-define('START_AT', 'adâncime');
+define('START_AT', 'afurisenie');
 
 $GRAMMAR = [
   'start' => [
@@ -181,6 +181,9 @@ $MEANING_GRAMMAR = [
 
 $ETYMOLOGY_GRAMMAR = [
   'start' => [
+    'rule+" "',
+  ],
+  'rule' => [
     'reference',
     'fromLang',
     'import',
@@ -192,12 +195,11 @@ $ETYMOLOGY_GRAMMAR = [
     'specials',
   ],
   'reference' => [
-    '"#V.# " formDot',
-    '"Din " formDot',
-    '"De la " formDot',
+    '("#V.# " | "Din " | "De la ") formDot',
   ],
   'fromLang' => [
-    '"Din " (lang " " formComma " ")* lang " " formDot',
+    '("Din " | "După ") (langList " " formComma " ")* langList " " formDot',
+    '"Din " langList " " formNone " " translation "."',
   ],
   'import' => [
     '"#Cuv.# " lang',
@@ -205,21 +207,26 @@ $ETYMOLOGY_GRAMMAR = [
     '"#Expr.# " lang',
   ],
   'withSuffix' => [
-    'formNone " + #suf.# " suffixDot',
+    'formNone elision? " + #suf.# " suffixDot',
+    'formNone " (#n. pr.#) + #suf.# " suffixDot',
+    'formNone " + #suf.# " suffixNone " (după " lang " " formItalicBracketDot',
   ],
   'glue' => [
-    '(formNone " + ")+ formDot',
+    '(formNone elision? " + ")+ formDot',
+    '(formNone elision? " + ")+ formNone elision "."',
+    'formNone+" + " " (după " lang " " formItalicBracketDot',
   ],
   'confer' => [
     '"#Cf.# " conferSourceDot',
-    '"#Cf.# " lang " " conferSourceDot',
+    '"#Cf.# " (lang " " conferSourceComma " ")* lang " " conferSourceDot',
   ],
   'regression' => [
     '"Din " formNone " (derivat regresiv)."',
   ],
   'latin' => [
     '"#Lat.# " formDot',
-    '"#Lat.# " formNone translation "."',
+    '"#Lat.# " formNone " " translation "."',
+    '"#Lat.# " formNone / \([=<] / formItalicBracketDot',
   ],
   'specials' => [
     '"Denumire comercială."',
@@ -228,26 +235,41 @@ $ETYMOLOGY_GRAMMAR = [
     '"Onomatopee."',
     '"Probabil formație onomatopeică."',
   ],
+  'langList' => [
+    'lang+", "',
+  ],
   'lang' => [
     '/#[^#]+#/',
   ],
   'formComma' => [
-    '/@[^@,]+,@/',
+    '/@[^@]+,@/',
   ],
   'formDot' => [
-    '/@[^@.]+\.@/',
+    '/@[^@]+\.@/',
   ],
   'formNone' => [
     '/@[^@]+@/',
   ],
+  'formItalicBracketDot' => [
+    '/\$[^$]+\)\.\$/',
+  ],
   'suffixDot' => [
-    '/\$-[^$.]+\.\$/',
+    '/\$-[^$]+\.\$/',
+  ],
+  'suffixNone' => [
+    '/\$-[^$]+\$/',
+  ],
+  'conferSourceComma' => [
+    '/%[^%]+,%/',
   ],
   'conferSourceDot' => [
-    '/%[^%.]+\.%/',
+    '/%[^%]+\.%/',
   ],
   'translation' => [
     '/"[^"]+"/',
+  ],
+  'elision' => [
+    '/\[[^]]+\]/',
   ],
 ];
 
@@ -266,12 +288,14 @@ $tagMap['argotic'] = $tagMap['argou; argotic'];
 $tagMap['articol; articulat'] = $tagMap['articulat'];
 $tagMap['colectiv'] = $tagMap['(cu sens) colectiv'];
 $tagMap['cu sens colectiv'] = $tagMap['(cu sens) colectiv'];
+$tagMap['din'] = $tagMap['(provine) din'];
 $tagMap['în expresie'] = $tagMap['expresie'];
 $tagMap['în locuțiune adverbială'] = $tagMap['locuțiune adverbială'];
 $tagMap['în sintagma'] = $tagMap['(în) sintagmă'];
 $tagMap['în sintagmele'] = $tagMap['(în) sintagmă'];
 $tagMap['la plural'] = $tagMap['(la) plural'];
 $tagMap['la singular'] = $tagMap['(la) singular'];
+$tagMap['(limba) sârbă, croată'] = $tagMap['limba sârbă, croată'];
 $tagMap['plural'] = $tagMap['(la) plural'];
 $tagMap['singular'] = $tagMap['(la) singular'];
 $tagMap['substantivat'] = $tagMap['(și) substantivat'];
@@ -308,8 +332,11 @@ do {
       if ($meaning) {
         $parsed = $meaningParser->parse($meaning);
         if ($parsed) {
-          createMeanings($parsed, $d);
-          createEtymologies($etymology, $etymologyParser, $d);
+          // $tuples1 = createMeanings($parsed, $d);
+          $tuples1 = [];
+          $tuples2 = createEtymologies($etymology, $etymologyParser, $d);
+          $tuples = array_merge($tuples1, $tuples2);
+          makeTree($tuples);
         } else {
           Log::error('Cannot parse meaning for [%s]: [%s]', $d->lexicon, $meaning);
         }
@@ -401,14 +428,13 @@ function createMeanings($parsed, $def) {
 
     // Log::info('%s: %s %s', $def->lexicon, implode(' ', $quals), $rep);
   }
+  return [];
 }
 
 // Parses qualifiers like "(#Înv.# și #pop.#)" before a meaning.
 // Returns a list of tags.
 // Modifies $rep when possible.
 function getQualifiers(&$rep) {
-  global $tagMap;
-
   $result = [];
 
   getAbbreviations($rep, $result);
@@ -440,15 +466,13 @@ function getQualifiers(&$rep) {
 
 // Remove abbreviations from the beginning, while we have tags for them.
 function getAbbreviations(&$rep, &$result) {
-  global $tagMap;
-
   do {
     $hash = false;
     if (preg_match('/^(și )?#([^#]+)#,? (.*)$/', $rep, $m)) {
       $abbr = mb_strtolower($m[2]);
       $exp = AdminStringUtil::getAbbreviation(SOURCE_ID, $abbr);
-      if (isset($tagMap[$exp])) {
-        $result[] = $tagMap[$exp];
+      if (getTag($exp, false)) {
+        $result[] = getTag($exp);
         $rep = $m[3];
         $hash = true;
       }
@@ -458,16 +482,10 @@ function getAbbreviations(&$rep, &$result) {
 
 // Returns a matching tag or null if there is no matching tag.
 function processQualifier($qual) {
-  global $tagMap;
-
   $qual = mb_strtolower($qual);
   $qual = expandAllAbbreviations($qual);
 
-  if (isset($tagMap[$qual])) {
-    return $tagMap[$qual];
-  } else {
-    return null;
-  }
+  return getTag($qual, false);
 }
 
 function expandAllAbbreviations($s) {
@@ -478,13 +496,157 @@ function expandAllAbbreviations($s) {
   return $s;
 }
 
+// returns an array of <meaning, tags> tuples
 function createEtymologies($etymology, $parser, $def) {
+
   if (!$etymology) {
-    return;
+    return [];
   }
+  $result = [];
+
   $etymology = (string)$etymology;
   $parsed = $parser->parse($etymology);
-  if (!$parsed) {
-    print "{$etymology}\n";
+
+  if ($parsed) {
+    // Create a tuple from every rule
+    foreach ($parsed->findAll('rule') as $rule) {
+
+      if ($r = $rule->findFirst('latin')) {
+        // '"#Lat.# " etc.'
+        $m = makeEtymology(substr($r, 7)); // skip "#Lat.# "
+        $tags = [ getTag('limba latină') ];
+        $result[] = [ 'meaning' => $m, 'tags' => $tags ];
+
+      } else if ($rule->findFirst('fromLang') && $rule->findFirst('translation')) {
+        // '"Din " langList " " formNone " " translation "."',
+        $tags = [];
+
+        foreach ($rule->findAll('lang') as $l) {
+          $tags[] = expandAndGetTag($l);
+        }
+        $m = makeEtymology(sprintf('%s %s',
+                                   $rule->findFirst('formNone'),
+                                   $rule->findFirst('translation')));
+        $result[] = [ 'meaning' => $m, 'tags' => $tags ];
+
+      } else if ($rule->findFirst('fromLang') && $rule->findFirst('formDot')) {
+        // '("Din " | "După ") (langList " " formComma " ")* langList " " formDot',
+        $fromLang = $rule->findFirst('fromLang');
+        $after = (string)$fromLang->getSubnode(0) == 'După ';
+
+        $list = $fromLang->findFirst('list');
+        if ($list) {
+          foreach ($list->getSubnodes() as $item) {
+            $tags = [];
+            if ($after) {
+              $tags[] = getTag('după');
+            }
+            foreach ($item->findAll('lang') as $l) {
+              $tags[] = expandAndGetTag($l);
+            }
+            $m = makeEtymology(preg_replace('/,@$/', '@', $item->findFirst('formComma')));
+            $result[] = [ 'meaning' => $m, 'tags' => $tags ];
+          }
+        }
+
+        $tags = [];
+        if ($after) {
+          $tags[] = getTag('după');
+        }
+        foreach ($fromLang->getSubnode(2)->findAll('lang') as $l) {
+          $tags[] = expandAndGetTag($l);
+        }
+        $m = makeEtymology(preg_replace('/\.@$/', '@', $fromLang->getSubnode(4)));
+          
+        $result[] = [ 'meaning' => $m, 'tags' => $tags ];
+
+      } else if ($rule->findFirst('reference')) {
+        // '("#V.# " | "Din " | "De la ") formDot',
+        $m = makeEtymology(preg_replace('/\.@$/', '@', $rule->findFirst('formDot')));
+        switch ($rule->findFirst('reference')->getSubnode(0)) {
+          case '#V.# ': $tags = [ getTag('vezi') ]; break;
+          case 'Din ': $tags = [ getTag('din') ]; break;
+          case 'De la ': $tags = [ getTag('de la') ]; break;
+        }
+        $result[] = [ 'meaning' => $m, 'tags' => $tags ];
+
+      } else if (($r = $rule->findFirst('withSuffix')) && $rule->findFirst('formItalicBracketDot')) {
+        // 'formNone " + #suf.# " suffixNone " (după " lang " " formItalicBracketDot',
+        $rep = sprintf('%s + sufix %s', $r->findFirst('formNone'), $r->findFirst('suffixNone'));
+        $m = makeEtymology(expandAllAbbreviations($rep));
+        $tags = [];
+        $result[] = [ 'meaning' => $m, 'tags' => $tags ];
+
+        $form = preg_replace('/\)\.\$$/', '$', $rule->findFirst('formItalicBracketDot'));
+        $rep = sprintf('%s %s', $r->findFirst('lang'), $form);
+        $m = makeEtymology(expandAllAbbreviations($rep));
+        $tags = [ getTag('după') ];
+        $result[] = [ 'meaning' => $m, 'tags' => $tags ];
+
+      } else if ($r = $rule->findFirst('withSuffix')) {
+        // other withSuffix rules
+        $m = makeEtymology(expandAllAbbreviations($r));
+        $tags = [];
+        $result[] = [ 'meaning' => $m, 'tags' => $tags ];
+        // printf("[%s] %s %s\n", $def->lexicon, implode(',', $tags), $m->internalRep);
+
+      } else {
+        print "*** [{$def->lexicon}] {$etymology}\n";
+        print $rule->dump() . "\n";
+        exit;
+      }
+    }
+  } else {
+    // Create a meaning using the whole text
+    $m = Model::factory('Meaning')->create();
+    $m->type = Meaning::TYPE_ETYMOLOGY;
+    $m->internalRep = $etymology;
+    $result[] = [
+      'meaning' => $m,
+      'tags' => [],
+    ];
   }
+
+  return $result;
+}
+
+function makeEtymology($internalRep) {
+  $m = Model::factory('Meaning')->create();
+  $m->type = Meaning::TYPE_ETYMOLOGY;
+  $m->internalRep = $internalRep;
+  return $m;
+}
+
+function makeTree($tuples) {
+  // TODO: figure out which tree to use
+  $t = null;
+
+  foreach ($tuples as $tuple) {
+    $m = $tuple['meaning'];
+    $m->parentId = 0;
+    $m->userId = MY_USER_ID;
+    // $m->treeId = $t->id; TODO
+    $m->htmlRep = AdminStringUtil::htmlize($m->internalRep, 0);    
+  }
+}
+
+// If no tag with the given value exists, then
+// * exists if $assert = true
+// * returns null if $assert = false
+function getTag($value, $assert = true) {
+  global $tagMap;
+
+  if (isset($tagMap[$value])) {
+    return $tagMap[$value];
+  } else if ($assert) {
+    Log::error("No tag: {$value}");
+    exit;
+  } else {
+    return null;
+  }
+}
+
+function expandAndGetTag($value) {
+  $value = expandAllAbbreviations($value);
+  return getTag($value);
 }

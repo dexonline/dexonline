29ec5de6b0d18bcd208c87b42106dd2cd8fed19d
Cătălin Frâncu <cata@francu.com>
2016-12-20 14:49:48 +0200

    Better meaning mentions for meanings with empty text.

--- wwwbase/ajax/meaningMention.php
+++ wwwbase/ajax/meaningMention.php
@@ -8,6 +8,7 @@ $data = Model::factory('InflectedForm')
       ->table_alias('if')
       ->select('t.description')
       ->select('m.id')
+      ->select('m.type')
       ->select('m.breadcrumb')
       ->select('m.htmlRep')
       ->distinct()
@@ -22,19 +23,41 @@ $data = Model::factory('InflectedForm')
                       ['m.id' => "{$qualifier}%"]],
                      'like')
       ->order_by_asc('t.description')
-      ->order_by_asc('m.breadcrumb')
+      ->order_by_asc('m.type')
+      ->order_by_asc('m.displayOrder')
       ->find_many();
 
 $results = [];
 foreach ($data as $r) {
+  $rep = $r->htmlRep;
+  if (!$rep && $r->type == Meaning::TYPE_MEANING) {
+    // empty meanings usually have synonyms
+    $relations = Relation::loadByMeaningId($r->id);
+    $synonyms = $relations[Relation::TYPE_SYNONYM];
+    $rep = sprintf('<i>sinonime:</i> %s',
+                   implode(', ', util_objectProperty($synonyms, 'description')));
+  }
+  else if (!$rep && $r->type == Meaning::TYPE_ETYMOLOGY) {
+    // empty etyomologies usually have tags
+    $tags = Model::factory('Tag')
+          ->table_alias('t')
+          ->join('ObjectTag', ['t.id', '=', 'ot.tagId'], 'ot')
+          ->where('ot.objectType', ObjectTag::TYPE_MEANING)
+          ->where('ot.objectId', $r->id)
+          ->order_by_asc('ot.id')
+          ->find_many();
+    $rep = sprintf('<i>etichete:</i> %s',
+                   implode(', ', util_objectProperty($tags, 'value')));
+  }
   $results[] = [
     'description' => $r->description,
-    'meaning' => $r->htmlRep,
+    'meaning' => $rep,
     'meaningId' => $r->id,
     'breadcrumb' => $r->breadcrumb,
   ];
 }
 
+header('Content-Type: application/json');
 print json_encode($results);
 
 ?>

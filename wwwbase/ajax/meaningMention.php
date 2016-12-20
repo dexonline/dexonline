ba9dbb5cf693238f71b653bd5176b0aee87d15fe
Cătălin Frâncu <cata@francu.com>
2016-12-20 15:31:53 +0200

    Show only proper meanings for mentions.

--- wwwbase/ajax/meaningMention.php
+++ wwwbase/ajax/meaningMention.php
@@ -19,35 +19,23 @@ $data = Model::factory('InflectedForm')
       ->join('Tree', ['te.treeId', '=', 't.id'], 't')
       ->join('Meaning', ['t.id', '=', 'm.treeId'], 'm')
       ->where('if.formNoAccent', $form)
+      ->where('m.type', Meaning::TYPE_MEANING)
       ->where_any_is([['m.breadcrumb' => "{$qualifier}%"],
                       ['m.id' => "{$qualifier}%"]],
                      'like')
       ->order_by_asc('t.description')
-      ->order_by_asc('m.type')
       ->order_by_asc('m.displayOrder')
       ->find_many();
 
 $results = [];
 foreach ($data as $r) {
   $rep = $r->htmlRep;
-  if (!$rep && $r->type == Meaning::TYPE_MEANING) {
+  if (!$rep) {
     // empty meanings usually have synonyms
     $relations = Relation::loadByMeaningId($r->id);
     $synonyms = $relations[Relation::TYPE_SYNONYM];
     $rep = sprintf('<i>sinonime:</i> %s',
                    implode(', ', util_objectProperty($synonyms, 'description')));
-  }
-  else if (!$rep && $r->type == Meaning::TYPE_ETYMOLOGY) {
-    // empty etyomologies usually have tags
-    $tags = Model::factory('Tag')
-          ->table_alias('t')
-          ->join('ObjectTag', ['t.id', '=', 'ot.tagId'], 'ot')
-          ->where('ot.objectType', ObjectTag::TYPE_MEANING)
-          ->where('ot.objectId', $r->id)
-          ->order_by_asc('ot.id')
-          ->find_many();
-    $rep = sprintf('<i>etichete:</i> %s',
-                   implode(', ', util_objectProperty($tags, 'value')));
   }
   $results[] = [
     'description' => $r->description,

2176071c4af78b989756d3e2f1739a8ed2a0979b
Cătălin Frâncu <cata@francu.com>
2016-12-26 16:26:11 +0200

    Fix collation on some fields to improve Ajax lookups.

---
+++ patches/00202.sql
@@ -0,0 +1,2 @@
+alter table Entry change description description varchar(255) not null collate utf8_romanian_ci;
+alter table Tree change description description varchar(255) not null collate utf8_romanian_ci;

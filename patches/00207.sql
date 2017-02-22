970f4b8e7c141c13225a155f5b2085936f2ef11b
Cătălin Frâncu <cata@francu.com>
2017-02-22 15:47:24 +0200

    Fix typo in SQL file. Addresses #299.

--- patches/00207.sql
+++ patches/00207.sql
@@ -1,2 +1,2 @@
-ALTER TABLE source ADD COLUMN remark varchar(255) COLLATE utf8_romanian_ci DEFAULT '' AFTER reformId;
-UPDATE source SET remark = 'cu ortografie modificată conform normelor din 1993', year = substr(year, 1, 4) WHERE id IN (3,4,5,15,16);
+ALTER TABLE Source ADD COLUMN remark varchar(255) COLLATE utf8_romanian_ci DEFAULT '' AFTER reformId;
+UPDATE Source SET remark = 'cu ortografie modificată conform normelor din 1993', year = substr(year, 1, 4) WHERE id IN (3,4,5,15,16);

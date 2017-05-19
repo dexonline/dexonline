206408fa0a180608e8a6552d7a5db1198d393503
Cătălin Frâncu <cata@francu.com>
2017-05-19 11:55:45 +0300

    Script to sanitize all meanings. Archive old script. Fixes #543.

---
+++ tools/sanitizeMeanings.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Sanitize all meanings
+ **/
+
+require_once __DIR__ . '/../phplib/Core.php';
+require_once __DIR__ . '/../phplib/third-party/PHP-parsing-tool/Parser.php';
+ini_set('memory_limit', '1024M');
+
+$meanings = Model::factory('Meaning')
+          ->order_by_asc('id')
+          ->find_many();
+
+foreach ($meanings as $i => $m) {
+  $rep = AdminStringUtil::sanitize($m->internalRep);
+  if ($rep != $m->internalRep) {
+    Log::info("Modified\n[%s] into\n[%s]", $m->internalRep, $rep);
+    $m->internalRep = $rep;
+    $m->htmlRep = AdminStringUtil::htmlize($rep, 0);
+    $m->save();
+  }
+  if ($i % 10000 == 0) {
+    Log::info('***** %s of %s meanings processed.', $i, count($meanings));
+  }
+}

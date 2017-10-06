9a64752fee2c34e4ace06ad53f62f23b33176568
Cătălin Frâncu <cata@francu.com>
2017-10-06 11:47:32 +0300

    Patch the patch. #612

--- patches/00232.sql
+++ patches/00232.sql
@@ -1,4 +1,4 @@
-alter table typo
+alter table Typo
     add column userName varchar(100) not null after problem;
 alter table User 
     change column preferences  preferences int(11) not null default '0' after moderator;

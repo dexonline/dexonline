17e6c77b95e7d72962ea69bad5099b68a5c8f0d8
Cătălin Frâncu <cata@francu.com>
2017-10-06 22:15:04 +0300

    Fix bug with anonymous typo reports. #612

--- wwwbase/ajax/typo.php
+++ wwwbase/ajax/typo.php
@@ -7,9 +7,11 @@ $text = Request::get('text');
 $definitionId = Request::get('definitionId');
 
 if ($text && $definitionId) {
+  $user = User::getActive();
+
   $typo = Model::factory('Typo')->create();
   $typo->definitionId = $definitionId;
   $typo->problem = $text;
-  $typo->userName = User::getActive();
+  $typo->userName = $user ? $user->nick : 'Anonim';
   $typo->save();
 }

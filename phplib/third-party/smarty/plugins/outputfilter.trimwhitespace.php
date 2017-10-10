728c5213eeb9882dc48f70cc12e4e09051d074ac
Cezar <cezar.elnazli2@gmail.com>
2017-10-10 11:09:04 +0300

    Anulat modificările scriptului third-party

--- phplib/third-party/smarty/plugins/outputfilter.trimwhitespace.php
+++ phplib/third-party/smarty/plugins/outputfilter.trimwhitespace.php
@@ -58,8 +58,15 @@ function smarty_outputfilter_trimwhitespace($source, Smarty_Internal_Template $s
     }
 
     $expressions = array(
-        '#\n#Ss' => '',
-        '#\s+#Ss' => ' ',
+        // replace multiple spaces between tags by a single space
+        // can't remove them entirely, becaue that might break poorly implemented CSS display:inline-block elements
+        '#(:SMARTY@!@|>)\s+(?=@!@SMARTY:|<)#s' => '\1 \2',
+        // remove spaces between attributes (but not in attribute values!)
+        '#(([a-z0-9]\s*=\s*(["\'])[^\3]*?\3)|<[a-z0-9_]+)\s+([a-z/>])#is' => '\1 \4',
+        // note: for some very weird reason trim() seems to remove spaces inside attributes.
+        // maybe a \0 byte or something is interfering?
+        '#^\s+<#Ss' => '<',
+        '#>\s+$#Ss' => '>',
     );
 
     $source = preg_replace( array_keys($expressions), array_values($expressions), $source );

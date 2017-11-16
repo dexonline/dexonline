9930bcc2e9b66b0002b67253b3a6128eeba32ee6
Cătălin Frâncu <cata@francu.com>
2017-11-16 16:34:43 +0200

    The unassociated lexeme report now takes fragments into account.

--- wwwbase/admin/viewUnassociatedLexems.php
+++ wwwbase/admin/viewUnassociatedLexems.php
@@ -3,13 +3,7 @@ require_once("../../phplib/Core.php");
 User::mustHave(User::PRIV_EDIT);
 Util::assertNotMirror();
 
-$lexems = Model::factory('Lexem')
-        ->table_alias('l')
-        ->select('l.*')
-        ->left_outer_join('EntryLexem', ['l.id', '=', 'el.lexemId'], 'el')
-        ->where_null('el.id')
-        ->order_by_asc('l.form')
-        ->find_many();
+$lexems = Lexem::getUnassociated();
 
 SmartyWrap::assign('lexems', $lexems);
 SmartyWrap::addCss('admin');

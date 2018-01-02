1ae7ef77cfa6d3b0ab34f4f36ce76cb13332f59c
Cătălin Frâncu <cata@francu.com>
2018-01-02 16:10:13 +0200

    Changes to the species matcher. #647.

--- tools/associateDfl.php
+++ tools/associateDfl.php
@@ -6,33 +6,44 @@
 require_once __DIR__ . '/../phplib/Core.php';
 
 define('SOURCE_ID', 63);
+define('START_AT', 'spiraea albiflora');
 
 $defs = Model::factory('Definition')
       ->where('sourceId', SOURCE_ID)
       ->where('status', Definition::ST_ACTIVE)
+      ->where_gte('lexicon', START_AT)
       ->order_by_asc('lexicon')
       ->find_many();
 
 foreach ($defs as $d) {
   
-  if (preg_match('/^@([A-Z][a-zéë]+) (x )?([-a-zë]+)@/', $d->internalRep, $matches)) {
+  if (preg_match('/^@(([A-Z][a-zéë]+) (x )?([-a-zë]+))@/', $d->internalRep, $matches)) {
     // Genus species
-    $genus = $matches[1];
-    $species = $matches[3];
-    printf("%s gen %s specie %s\n", $matches[0], $genus, $species);
+    $entryName = $matches[1];
+    $genus = $matches[2];
+    $species = $matches[4];
+    $hybrid = ($matches[3] != '');
+    printf("%s gen %s specie %s %s\n",
+           $entryName, $genus, $species, $hybrid ? 'hibrid' : '');
+
+    // load or create the entry and associate it with the definition
+    $e = Entry::get_by_description($entryName);
+    if (!$e) {
+      printf("Creez o intrare pentru %s\n", $entryName);
+      $e = Entry::createAndSave($entryName);
+    }
+    EntryDefinition::associate($e->id, $d->id);
 
     // load the genus lexeme
     $glexeme = Lexem::get_by_formNoAccent_modelType_modelNumber($genus, 'I', '2.1');
     $glexeme or die("Nu găsesc lexemul pentru gen.\n");
 
-    // look for a species lexeme
-    $slexemes = Model::factory('Lexem')
-              ->where_raw('(binary formNoAccent like ?)', [$species])
-              ->where_in('modelType', ['T', 'I'])
-              ->find_many();
-    if (count($slexemes) > 1) {
-      printf("%d lexeme %s\n", count($slexemes), defUrl($d));
-    }
+    // load and update the species lexeme, or create it if it doesn't exist
+    $slexeme = getSpeciesLexeme($species);
+    EntryLexem::dissociate($e->id, $slexeme->id);
+
+    // look for a compound lexeme
+    $clexeme = Lexem::get_by_form_compound($entryName, true);
   }
 }
 
@@ -51,4 +62,27 @@ function choice($prompt, $choices) {
     $choice = readline($prompt . ' ');
   } while (!in_array($choice, $choices));
   return $choice;
+}
+
+function getSpeciesLexeme($species) {
+  $l = Model::factory('Lexem')
+    ->where_raw('(binary form like ?)', [$species])
+    ->where_any_is([
+      [ 'modelType' => 'T', 'modelNumber' => '1'],
+      [ 'modelType' => 'I', 'modelNumber' => '1'],
+      [ 'modelType' => 'I', 'modelNumber' => '2'],
+      [ 'modelType' => 'I', 'modelNumber' => '2.2'],
+    ])
+    ->find_one();
+  if ($l) {
+    $l->modelType = 'I';
+    $l->modelNumber = '2.2';
+  } else {
+    printf("Creez un lexem pentru %s\n", $species);
+    $l = Lexem::create($species, 'I', '2.2');
+  }
+  $l->description = 'specie de plante';
+  $l->noAccent = true;
+  $l->deepSave();
+  return $l;
 }

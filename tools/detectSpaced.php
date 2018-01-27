ee2196034bd24b7dcdfe1950b3e2903432371f2e
Cătălin Frâncu <cata@francu.com>
2018-01-27 11:10:00 +0200

    More patterns.

--- tools/detectSpaced.php
+++ tools/detectSpaced.php
@@ -6,12 +6,19 @@
 
 require_once __DIR__ . '/../phplib/Core.php';
 
-const START_ID = 534000;
+// const START_ID = 0;
+const START_ID = 821000;
 const BATCH_SIZE = 10000;
+const INCLUDE_SOURCES = [57];
 const EXCLUDE_SOURCES = [32];
 
-const ONOM_PATTERN = '/^@[^@]+@ v\.(( \p{L})+)( @[^@]+@)?$/u';
-const PATTERN = '/\b([@#$(\[]*\p{L}[@#$)\]]*\s){4,}/u';
+// Onomastic: @Actimie@ v. E f t i m i e @III 2.@
+const PATTERN1 = '/^@[^@]+@ v\.(( \p{L})+)( @[^@]+@)?$/u';
+
+//@Deodor@ v. D i o d o r.
+const PATTERN2 = '/^@[^@]+@ v\.(( \p{L})+)\\.?$/u';
+
+const PATTERN3 = '/\b([@#$(\[]*\p{L}[@#$)\]]*\s){4,}/u';
 
 $offset = 0;
 $found = 0;
@@ -20,6 +27,7 @@ do {
   $defs = Model::factory('Definition')
         ->where_in('status', [0, 3])
         ->where_gte('id', START_ID)
+        //        ->where_in('sourceId', INCLUDE_SOURCES)
         ->where_not_in('sourceId', EXCLUDE_SOURCES)
         ->order_by_asc('id')
         ->limit(BATCH_SIZE)
@@ -28,18 +36,35 @@ do {
 
   foreach ($defs as $d) {
     $matches = [];
-    if (preg_match(ONOM_PATTERN, $d->internalRep, $matches, PREG_OFFSET_CAPTURE)) {
+    $s = $d->internalRep;
+    $case = 0;
+
+    if (preg_match(PATTERN1, $d->internalRep, $matches, PREG_OFFSET_CAPTURE)) {
       $at = $matches[1][1];
       $len = strlen($matches[1][0]);
       $text = sprintf(' %%%s%%', str_replace(' ', '', $matches[1][0]));
-      $s = $d->internalRep;
       $d->internalRep = substr($s, 0, $at) . $text . substr($s, $at + $len);
 
-      printf("%s %s\n", defUrl($d), $d->internalRep);
-      printf("[%50s] -> [%s]\n", $s, $d->internalRep);
+      $case = 1;
 
+    } else if (preg_match(PATTERN2, $d->internalRep, $matches, PREG_OFFSET_CAPTURE)) {
+      $at = $matches[1][1];
+      $len = strlen($matches[1][0]);
+      $text = sprintf(' %%%s%%', str_replace(' ', '', $matches[1][0]));
+      $d->internalRep = substr($s, 0, $at) . $text . substr($s, $at + $len);
+
+      $case = 2;
+
+    } else if (preg_match_all(PATTERN3, $d->internalRep, $matches, PREG_OFFSET_CAPTURE)) {
+      printf("%s %s\n", defUrl($d), $d->internalRep);
       $found++;
-    } else if (preg_match_all(PATTERN, $d->internalRep, $matches, PREG_OFFSET_CAPTURE)) {
+    }
+
+    if ($case) {
+      printf("%s case %d [%s] -> [%s]\n", defUrl($d), $case, $s, $d->internalRep);
+      $d->htmlRep = Str::htmlize($d->internalRep, $d->sourceId);
+      $d->save();
+      $found++;
     }
   }
 

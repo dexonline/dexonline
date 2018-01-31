73c5fc0196e31e2ac4439cb59b5e0ae04d442460
Cătălin Frâncu <cata@francu.com>
2018-01-31 11:41:00 +0200

    Grant donor medals and hide banners automatically. #680.

---
+++ patches/00244.sql
@@ -0,0 +1,2 @@
+alter table User
+  change email email varchar(255) collate utf8_general_ci;
--- tools/updateMedals.php
+++ tools/updateMedals.php
@@ -19,6 +19,12 @@ const VOLUNTEER_LEVELS = [
   Medal::MEDAL_VOLUNTEER_1 => 1000,
 ];
 
+const MIN_DONATION_FOR_MEDAL = 20;
+const MIN_DONATION_FOR_HIDDEN_BANNERS = 50;
+
+// user IDs of people for whom we never want to hide banners
+const MUST_SEE_BANNERS = [ 1 ];
+
 Log::notice('started');
 
 $opts = getopt('dnov');
@@ -46,17 +52,17 @@ if (!$skipOtrs) {
       if ($user && $user->id) {
         if (($r['count'] >= 1000) &&
             (!($user->medalMask & Medal::MEDAL_EMAIL_3))) {
-          Log::info("Granting {$user->nick} a MEDAL_EMAIL_3");
+          Log::info("Granting {$user->id} {$user->nick} a MEDAL_EMAIL_3");
           $user->medalMask |= Medal::MEDAL_EMAIL_3;
         } else if (($r['count'] < 1000) &&
                    ($r['count'] >= 500) &&
                    (!($user->medalMask & Medal::MEDAL_EMAIL_2))) {
-          Log::info("Granting {$user->nick} a MEDAL_EMAIL_2");
+          Log::info("Granting {$user->id} {$user->nick} a MEDAL_EMAIL_2");
           $user->medalMask |= Medal::MEDAL_EMAIL_2;
         } else if (($r['count'] < 500) &&
                    ($r['count'] >= 100) &&
                    (!($user->medalMask & Medal::MEDAL_EMAIL_1))) {
-          Log::info("Granting {$user->nick} a MEDAL_EMAIL_1");
+          Log::info("Granting {$user->id} {$user->nick} a MEDAL_EMAIL_1");
           $user->medalMask |= Medal::MEDAL_EMAIL_1;
         }
         $user->medalMask = Medal::getCanonicalMask($user->medalMask);
@@ -86,13 +92,56 @@ if (!$skipVolunteers) {
 
       // grant it if the user doesn't have it
       if (!($user->medalMask & $medal)) {
-        Log::info('Granting %s a medal for %s', $user->nick, Medal::getName($medal));
+        Log::info('Granting %s %s a medal for %s', $user->id, $user->nick, Medal::getName($medal));
         $user->medalMask |= $medal;
         $user->medalMask = Medal::getCanonicalMask($user->medalMask);
         if (!$dryRun) {
           $user->save();
         }
       }
+    }
+  }
+}
+
+// Donor perks
+if (!$skipDonors) {
+  // Grant medals to users who don't have one, but have donated enough
+  $needMedals = Model::factory('User')
+              ->table_alias('u')
+              ->select('u.*')
+              ->select_expr('sum(d.amount)', 'total')
+              ->distinct()
+              ->join('Donation', ['d.email', '=', 'u.email'], 'd')
+              ->where_raw('!(medalMask & ?)', Medal::MEDAL_SPONSOR)
+              ->group_by('u.id')
+              ->having_raw('total >= ?', MIN_DONATION_FOR_MEDAL)
+              ->find_many();
+  foreach ($needMedals as $u) {
+    Log::info("Granting {$user->id} {$u->nick} a sponsor medal");
+    $u->medalMask |= Medal::MEDAL_SPONSOR;
+    if (!$dryRun) {
+      $u->save();
+    }
+  }
+
+  // Hide banners for users who have donated recently (since their banners were last hidden)
+  $oneYearFromNow = strtotime('+1 year');
+  $noBanners = Model::factory('User')
+             ->table_alias('u')
+             ->select('u.*')
+             ->select_expr('sum(d.amount)', 'total')
+             ->distinct()
+             ->join('Donation', ['d.email', '=', 'u.email'], 'd')
+             ->where_not_in('u.id', MUST_SEE_BANNERS)
+             ->where_raw('d.createDate >= u.noAdsUntil')
+             ->group_by('u.id')
+             ->having_raw('total >= ?', MIN_DONATION_FOR_HIDDEN_BANNERS)
+             ->find_many();
+  foreach ($noBanners as $u) {
+    Log::info("Hiding banners for {$u->id} {$u->nick}");
+    $u->noAdsUntil = $oneYearFromNow;
+    if (!$dryRun) {
+      $u->save();
     }
   }
 }

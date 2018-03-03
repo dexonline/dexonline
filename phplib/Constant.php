940ebe8e5306a2ef521bdbd3beaabb94a3de0455
Ladislau Strifler <ladislau.strifler@bbcomputer.ro>
2018-03-03 20:19:15 +0200

    Match newLine also.

--- phplib/Constant.php
+++ phplib/Constant.php
@@ -47,7 +47,7 @@ class Constant {
 
   // will use preg_replace for string values, preg_replace_callback for arrays
   const HTML_PATTERNS = [
-    '/▶(.*?)◀/' => '',                                              // remove unwanted parts of definition
+    '/▶(.*?)◀/s' => '',                                              // remove unwanted parts of definition
     '/(?<!\\\\)"([^"]*)"/' => '„$1”',                               // "x" => „x” - romanian quoting style
     '/(?<!\\\\)\{\{(.*)\}\}/U' => ['FootnoteHtmlizer', 'htmlize'], // {{fotnote}}
     '/(?<!\\\\)#([^#]*)#/' => ['AbbrevHtmlizer', 'htmlize'],       // #abbreviation#

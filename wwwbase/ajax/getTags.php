180450fbbf2e508dedcd9524f6d5d62b1d4de29e
Cătălin Frâncu <cata@francu.com>
2018-04-18 14:39:18 +0300

    [#724] Prefer prefix matches in select2 tag lookups.

--- wwwbase/ajax/getTags.php
+++ wwwbase/ajax/getTags.php
@@ -2,9 +2,11 @@
 require_once("../../phplib/Core.php");
 
 $query = Request::get('term');
+$query = addslashes($query);
 
 $tags = Model::factory('Tag')
       ->where_like('value', "%{$query}%")
+      ->order_by_expr("value like '{$query}%' desc") // prefer prefix matches
       ->order_by_asc('value')
       ->limit(10)
       ->find_many();

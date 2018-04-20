c97d48f4859100bcfd4125c2bc03805273bcb618
Cătălin Frâncu <cata@francu.com>
2018-04-20 15:37:41 +0300

    [#698] Preserve accent position when the model gives no explicit information.

--- phplib/FlexStr.php
+++ phplib/FlexStr.php
@@ -420,13 +420,14 @@ class FlexStr {
     }
 
     // Try to place the accent
-    if ($accentShift == ModelDescription::NO_ACCENT_SHIFT) {
+    if (($accentShift == ModelDescription::NO_ACCENT_SHIFT) ||
+        ($accentShift == ModelDescription::UNKNOWN_ACCENT_SHIFT)) {
       // Place the accent exactly where it is in the lexeme form, if there is
       // one.
       if ($accentIndex !== false) {
         $result = substr_replace($result, "'", $accentIndex, 0);
       }
-    } else if ($accentShift != ModelDescription::UNKNOWN_ACCENT_SHIFT) {
+    } else {
       $result = self::placeAccent($result, $accentShift, $accentedVowel);
     }
     return $result;

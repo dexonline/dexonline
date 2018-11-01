f2bfcbbe29acf123c40ee9aac173bdd5253fb718
Cătălin Frâncu <cata@francu.com>
2018-11-01 17:01:50 +0200

    [#766] Make tree mentions navigate to the tree tab.

--- phplib/htmlize/MentionHtmlizer.php
+++ phplib/htmlize/MentionHtmlizer.php
@@ -20,10 +20,39 @@ class MentionHtmlizer extends Htmlizer {
       case 2: $contents = "(<b>$bc</b>)"; break;
     }
 
-    $result = sprintf(
-      '<span data-toggle="popover" data-html="true" data-placement="auto right" ' .
-      'class="mention" title="%s">%s</span>',
-      $meaningId, $contents);
+    // Figure out the landing page that contains this meaning. Load an entry
+    // associated with this meaning, preferably one that generates the form in $text.
+    $statuses = [Entry::STRUCT_STATUS_DONE, Entry::STRUCT_STATUS_UNDER_REVIEW];
+    $entry = Model::factory('Entry')
+      ->select('e.*')
+      ->table_alias('e')
+      ->join('TreeEntry', ['e.id', '=', 'te.entryId'], 'te')
+      ->join('Tree', ['te.treeId', '=', 't.id'], 't')
+      ->join('EntryLexeme', ['e.id', '=', 'el.entryId'], 'el')
+      ->join('InflectedForm', ['el.lexemeId', '=', 'i.lexemeId'], 'i')
+      ->where('t.id', $m->treeId)
+      ->where('t.status', Tree::ST_VISIBLE)
+      ->where_in('e.structStatus', $statuses)
+      ->order_by_expr(sprintf('i.formNoAccent = "%s" desc', addslashes($text)))
+      ->find_one();
+
+    $attributes = sprintf(
+      'data-toggle="popover" ' .
+      'data-html="true" ' .
+      'data-placement="auto right" ' .
+      'class="mention" ' .
+      'title="%s"', $meaningId);
+    if ($entry) {
+      $href = sprintf('%sintrare/%s/%d/sinteza#meaning%d',
+                      Core::getWwwRoot(),
+                      $entry->getShortDescription(),
+                      $entry->id,
+                      $meaningId);
+      $result = sprintf('<a href="%s" %s>%s</a>', $href, $attributes, $contents);
+    } else {
+      $result = sprintf('<span %s>%s</span>', $attributes, $contents);
+    }
+
     return $result;
   }
 }

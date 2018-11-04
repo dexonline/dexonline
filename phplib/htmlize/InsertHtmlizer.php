8144f59875c9535250a6eed50cfcf0365c53a514
Ladislau STRIFLER <ladislau.strifler@bbcomputer.ro>
2018-11-04 16:40:14 +0200

    Make single-line regexp for del and ins htmlizers. (#775)
    Definition history does not need diff.js anymore.
    Create app-wide different SPACE constants.
    Improved DeleteHtmlizer.

--- phplib/htmlize/InsertHtmlizer.php
+++ phplib/htmlize/InsertHtmlizer.php
@@ -4,7 +4,7 @@ class InsertHtmlizer extends Htmlizer {
 
   // htmlize one inserted chunk formatted as {+text+}
   function htmlize($match) {
-    $match = str_replace(" ","␣", $match);
+    $match = str_replace(Constant::SPACES['regular'], Constant::OPENBOX . Constant::SPACES['hair'], $match);
     return sprintf('<ins>%s</ins>', $match[1]);
   }
 }

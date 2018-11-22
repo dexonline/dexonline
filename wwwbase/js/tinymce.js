189111402ee1749f5284ae2ba146aa3f22549a55
Ladislau STRIFLER <ladislau.strifler@bbcomputer.ro>
2018-11-22 16:01:01 +0200

    Fix for #787. (#788)
    * Fix for #787.
    Added abbreviation coloring and syntax wrapper.
    
    * fixed comments

--- wwwbase/js/tinymce.js
+++ wwwbase/js/tinymce.js
@@ -23,7 +23,7 @@ $(function() {
         resize: 'both',
         selector: '.tinymceTextarea',
         setup: tinymceSetup,
-        toolbar: 'undo redo | bold italic spaced superscript subscript',
+        toolbar: 'undo redo | bold italic spaced superscript subscript abbrev',
         width: '100%',
       });
       $.cookie(TINYMCE_COOKIE, 'on', { expires: 3650, path: '/' });
@@ -50,10 +50,18 @@ $(function() {
       // In this case evt.target will be .mce-content-body.
       editor.on('keydown', function(evt) { _doc.trigger(evt); });
 
+      // Set fontSize slightly bigger than default 11px
+      editor.getBody().style.fontSize = '14px';
+
       // Register a "spaced" format
       editor.formatter.register('spaced', {
         inline : 'span',
         classes: 'spaced',
+      });
+
+      // Register an "abbrev" format
+      editor.formatter.register('abbrev', {
+        inline : 'abbr',
       });
 
       // Add a shortcut for toggling the spaced format
@@ -86,12 +94,31 @@ $(function() {
         editor.formatter ? setup() : editor.on('init', setup);
       }
     });
+
+    // Add a toolbar button for abbreviated text
+    editor.addButton('abbrev', {
+      tooltip: 'Abreviere',
+      text: '#',
+      onClick: function() {
+        editor.formatter.toggle('abbrev');
+      },
+      onPostRender: function() {
+        var self = this, setup = function() {
+          editor.formatter.formatChanged('abbrev', function(state) {
+            self.active(state);
+          });
+        };
+        editor.formatter ? setup() : editor.on('init', setup);
+      }
+    });
   }
 
     // Convert some of our internal notation to HTML. This is not exhaustive,
     // just enough to allow TinyMCE to work properly.
   function internalToHtml(ed) {
     var s = $('#' + ed.target.id).val();
+    s = '<p>' + s.replace(/\n{1,}/gi, '</p><p>') + '</p>'; // wrap paragraphs
+
     s = s.replace(/\\@/g, '~~~SAVE~~~'); // move \@ out of the way
     s = s.replace(/@([^@]*)@/g, '<strong>$1</strong>');
     s = s.replace(/~~~SAVE~~~/g, '\\@'); // restore \@
@@ -104,6 +131,10 @@ $(function() {
     s = s.replace(/%([^%]*)%/g, '<span class="spaced">$1</span>');
     s = s.replace(/~~~SAVE~~~/g, '\\%'); // restore \%
 
+    s = s.replace(/\\#/g, '~~~SAVE~~~'); // move \# out of the way
+    s = s.replace(/#([^#]*)#/g, '<abbr>$1</abbr>');
+    s = s.replace(/~~~SAVE~~~/g, '\\#'); // restore \#
+
     s = s.replace(/\^(\d)/g, '<sup>$1</sup>');
     s = s.replace(/_(\d)/g, '<sub>$1</sub>');
     s = s.replace(/\^\{([^}]*)\}/g, '<sup>$1</sup>');
@@ -114,10 +145,11 @@ $(function() {
   // Convert HTML to our internal notation
   function htmlToInternal(ed) {
     var s = ed.content;
-    s = s.replace(/<\/?p>/gi, '');
+    s = s.replace(/<\/p><p>/gi, '\n').replace(/<\/?p>/gi, '');
     s = s.replace(/<\/?strong>/gi, '@');
     s = s.replace(/<\/?em>/gi, '$');
     s = s.replace(/<span class="spaced">(.*?)<\/span>/gi, '%$1%');
+    s = s.replace(/<abbr>(.*?)<\/abbr>/gi, '#$1#');
     s = s.replace(/<sup>(\d)<\/sup>/gi, '^$1');
     s = s.replace(/<sub>(\d)<\/sub>/gi, '_$1');
     s = s.replace(/<sup>(.*?)<\/sup>/gi, '^{$1}'); // *? = non-greedy

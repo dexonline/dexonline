85cbf5ff8a1b7d34b892aa0f9cb9a1cb0b154f3b
Cătălin Frâncu <cata@francu.com>
2019-05-22 14:27:16 +0300

    Remove unused function.

--- lib/OS.php
+++ lib/OS.php
@@ -27,18 +27,6 @@ class OS {
     }
   }
 
-  static function executeAndReturnOutput($command) {
-    $exit_code = 0;
-    $output = null;
-    exec($command, $output, $exit_code);
-    if ($exit_code) {
-      print("ERROR: Failed command: $command (code $exit_code)\n");
-      var_dump($output);
-      exit;
-    }
-    return $output;
-  }
-
   /** Checks if the directory specified in $path is empty */
   static function isDirEmpty($path) {
     $files = scandir($path);

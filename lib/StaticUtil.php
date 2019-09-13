6732fc480c2911fc6bfe69de1a4bd51de700065d
Cătălin Frâncu <cata@francu.com>
2019-09-13 12:34:34 +0300

    Fix wotd images permissions once and for all (yeah).

--- lib/StaticUtil.php
+++ lib/StaticUtil.php
@@ -11,23 +11,30 @@ class StaticUtil {
     exec('find * | sort > fileList.txt');
   }
 
+  static function mkdir($path) {
+    @mkdir($path, 0777, true);
+    chmod($path, 0777);
+  }
+
   static function ensureThumb($src, $dest, $size) {
     $fullSrc = Config::STATIC_PATH . $src;
     $fullDest = Config::STATIC_PATH . $dest;
     if (!file_exists($fullDest)) {
-      @mkdir(dirname($fullDest), 0777, true);
+      self::mkdir(dirname($fullDest));
       $command = sprintf(
         "convert -strip -geometry %sx%s -sharpen 1x1 '%s' '%s'",
         $size, $size, $fullSrc, $fullDest);
       OS::execute($command);
+      chmod($fullDest, 0644);
     }
   }
 
   // $src is a file anywhere on the disk, while $dest is a subpath of the static server
   static function copy($src, $dest) {
     $fullDest = Config::STATIC_PATH . $dest;
-    @mkdir(dirname($fullDest), 0777, true);
+    self::mkdir(dirname($fullDest));
     copy($src, $fullDest);
+    chmod($fullDest, 0644);
   }
 
   // similar to copy, but also deletes $src
@@ -38,8 +45,9 @@ class StaticUtil {
 
   static function putContents(&$contents, $dest) {
     $fullDest = Config::STATIC_PATH . $dest;
-    @mkdir(dirname($fullDest), 0777, true);
+    self::mkdir(dirname($fullDest));
     file_put_contents($fullDest, $contents);
+    chmod($fullDest, 0644);
   }
 
   static function delete($file) {

8ac98d2ba061bf5a2cd31e07b3f8e3a1666e8400
Cătălin Frâncu <cata@francu.com>
2019-10-04 10:53:49 +0300

    Raised logging level for ignoring typos.

--- www/ajax/ignoreTypo.php
+++ www/ajax/ignoreTypo.php
@@ -5,6 +5,6 @@ User::mustHave(User::PRIV_EDIT);
 $typoId = Request::get('id');
 $typo = Typo::get_by_id($typoId);
 if ($typo) {
-  Log::debug("Ignored typo {$typo->id} ({$typo->problem}) reported by [{$typo->userName}]");
+  Log::notice("Ignored typo {$typo->id} ({$typo->problem}) reported by [{$typo->userName}]");
   $typo->delete();
 }

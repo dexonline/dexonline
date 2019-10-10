82b98275162516b3a4dece7ed24a56f32b1278ae
Radu Borza <radu_borza@yahoo.it>
2019-10-11 02:01:38 +0300

    Link artist to user account

---
+++ patches/00299.sql
@@ -0,0 +1,15 @@
+ALTER TABLE WotdArtist ADD COLUMN userId INT(11) DEFAULT NULL AFTER email;
+UPDATE WotdArtist SET userId = 38071 WHERE id = 2; #Carmen Nistor
+UPDATE WotdArtist SET userId = 40347 WHERE id = 7; #Andrea Homorodean
+#Vasile Alboiu - nu avem
+UPDATE WotdArtist SET userId = 40176 WHERE id = 20; #Ioana Adelina Boroș
+UPDATE WotdArtist SET userId = 39215 WHERE id = 5; #Gabriel Miu
+UPDATE WotdArtist SET userId = 39403 WHERE id = 16; #Teodora Pașcanu (are două conturi, și 39375)
+UPDATE WotdArtist SET userId = 40436 WHERE id = 13; #Tiberiu Teodor-Stanciu
+UPDATE WotdArtist SET userId = 40591 WHERE id = 12; #Ramona Costea
+UPDATE WotdArtist SET userId = 40386 WHERE id = 14; #Alexandra Berdan
+UPDATE WotdArtist SET userId = 40578 WHERE id = 27; #Vlad Enculescu
+UPDATE WotdArtist SET userId = 36184 WHERE id = 3; #Radu Borza
+UPDATE WotdArtist SET userId = 40592 WHERE id = 24; #Ștefania Lupan
+UPDATE WotdArtist SET userId = 29474 WHERE id = 8; #Marion Minkenberg (e singura Marion)
+UPDATE WotdArtist SET userId = 40825 WHERE id = 32; #Raisa Enculescu

bd542e0cfecdbf41f7479c4252ed162c08870172
Radu Borza <radu_borza@yahoo.it>
2019-11-02 02:22:57 +0200

    Refactorization Medals - final

--- lib/Medal.php
+++ lib/Medal.php
@@ -1,6 +1,7 @@
 <?php
 
 class Medal {
+  /* all medals (values in DB) */
   const MEDAL_SPONSOR = 0x1;
   const MEDAL_SOCIAL = 0x2;
   const MEDAL_ARTICLES = 0x4;
@@ -21,6 +22,45 @@ class Medal {
   const MEDAL_ARTIST_2 = 0x20000;
   const MEDAL_ARTIST_3 = 0x40000;
 
+  /* simple medals */
+  const SPONSOR_TEMPLATE = [
+    'name' => 'Donator',
+    'description' => '',
+    'pic' => 'sponsor.png',
+    'supersedes' => [],
+  ];
+
+  const SOCIAL_TEMPLATE = [
+    'name' => 'Activist pe rețele sociale',
+    'description' => '',
+    'pic' => 'social.png',
+    'supersedes' => [],
+  ];
+
+  const ARTICLES_TEMPLATE = [
+    'name' => 'Editor de articole lingvistice',
+    'description' => '',
+    'pic' => 'articles.png',
+    'supersedes' => [],
+  ];
+
+  const WOTD_TEMPLATE = [
+    'name' => 'Editor al cuvântului zilei',
+    'description' => '',
+    'pic' => 'wotd.png',
+    'supersedes' => [],
+  ];
+
+  const MODERATOR_TEMPLATE = [
+    'name' => 'Moderator',
+    'description' => '',
+    'pic' => 'moderator.png',
+    'supersedes' => [],
+  ];
+
+  /*  medals with more levels */
+
+  // programmers medals
   const PROGRAMMER_LEVELS = [
     Medal::MEDAL_PROGRAMMER_1 => 100,
     Medal::MEDAL_PROGRAMMER_2 => 1000,
@@ -31,8 +71,10 @@ class Medal {
     'name' => 'Programator (nivel %d)',
     'description' => 'peste %s de linii de cod',
     'pic' => 'programmer%d.png',
+    'supersedes' => [],
   ];
 
+  // email medals
   const EMAIL_LEVELS = [
     Medal::MEDAL_EMAIL_1 => 100,
     Medal::MEDAL_EMAIL_2 => 500,
@@ -43,8 +85,10 @@ class Medal {
     'name' => 'Responsabil e-mail (nivel %d)',
     'description' => 'peste %s de mesaje procesate',
     'pic' => 'email%d.png',
+    'supersedes' => [],
   ];
 
+  // editor medals
   const EDITOR_LEVELS = [
     Medal::MEDAL_EDITOR_1 => 1000,
     Medal::MEDAL_EDITOR_2 => 10000,
@@ -57,8 +101,10 @@ class Medal {
     'name' => 'Editor (nivel %d)',
     'description' => 'peste %s de caractere trimise',
     'pic' => 'editor%d.png',
+    'supersedes' => [],
   ];
 
+  //artist medals
   const ARTIST_LEVELS = [
     Medal::MEDAL_ARTIST_1 => 10,
     Medal::MEDAL_ARTIST_2 => 100,
@@ -69,7 +115,35 @@ class Medal {
     'name' => 'Desenator al cuvântului zilei (nivel %d)',
     'description' => 'minimum %s cuvinte ilustrate',
     'pic' => 'artist%d.png',
+    'supersedes' => [],
   ];
+
+  /**************************** what medals to display ***********************/
+  const SIMPLE_MEDALS = [
+    Medal::MEDAL_SPONSOR    => Medal::SPONSOR_TEMPLATE,
+    Medal::MEDAL_SOCIAL     => Medal::SOCIAL_TEMPLATE,
+    Medal::MEDAL_ARTICLES   => Medal::ARTICLES_TEMPLATE,
+    Medal::MEDAL_WOTD       => Medal::WOTD_TEMPLATE,
+    Medal::MEDAL_MODERATOR  => Medal::MODERATOR_TEMPLATE,
+  ];
+
+  const MEDALS_WITH_LEVELS = [
+    ['level' => Medal::PROGRAMMER_LEVELS, 'template' => Medal::PROGRAMMER_TEMPLATE],
+    ['level' => Medal::EMAIL_LEVELS ,     'template' => Medal::EMAIL_TEMPLATE],
+    ['level' => Medal::EDITOR_LEVELS ,    'template' => Medal::EDITOR_TEMPLATE],
+    ['level' => Medal::ARTIST_LEVELS ,    'template' => Medal::ARTIST_TEMPLATE],
+  ];
+  /*************************************************************************/
+
+  static function getData() {
+    $medalData = [];
+    $medalData += self::SIMPLE_MEDALS;
+    foreach (self::MEDALS_WITH_LEVELS as $medalType) {
+      $medalData += self::getMedalsDataFor($medalType['level'], $medalType['template']);
+    }
+
+    return $medalData;
+  }
 
   private static function getMedalsDataFor($levels, $template) {
     $levelCnt = 0;
@@ -87,50 +161,6 @@ class Medal {
     return $medals;
   }
 
-  static function getData() {
-    $medalData = [];
-
-    $simpleMedals = [
-      self::MEDAL_SPONSOR => [
-        'name' => 'Donator',
-        'description' => '',
-        'pic' => 'sponsor.png',
-        'supersedes' => [],
-      ],
-      self::MEDAL_SOCIAL => [
-        'name' => 'Activist pe rețele sociale',
-        'description' => '',
-        'pic' => 'social.png',
-        'supersedes' => [],
-      ],
-      self::MEDAL_ARTICLES => [
-        'name' => 'Editor de articole lingvistice',
-          'description' => '',
-          'pic' => 'articles.png',
-          'supersedes' => [],
-        ],
-      self::MEDAL_WOTD => [
-        'name' => 'Editor al cuvântului zilei',
-        'description' => '',
-        'pic' => 'wotd.png',
-        'supersedes' => [],
-      ],
-      self::MEDAL_MODERATOR => [
-        'name' => 'Moderator',
-        'description' => '',
-        'pic' => 'moderator.png',
-        'supersedes' => [],
-      ]
-    ];
-    $medalData += $simpleMedals;
-    $medalData += self::getMedalsDataFor(self::PROGRAMMER_LEVELS, self::PROGRAMMER_TEMPLATE);
-    $medalData += self::getMedalsDataFor(self::EMAIL_LEVELS, self::EMAIL_TEMPLATE);
-    $medalData += self::getMedalsDataFor(self::EDITOR_LEVELS, self::EDITOR_TEMPLATE);
-    $medalData += self::getMedalsDataFor(self::ARTIST_LEVELS, self::ARTIST_TEMPLATE);
-
-    return $medalData;
-  }
-
   /* Returns a subset of DATA */
   static function loadForUser($user) {
     $result = [];
@@ -143,8 +173,8 @@ class Medal {
     return $result;
   }
 
-  /* Returns the canonical mask for a given mask, removing values which are
-     superseded by other values */
+  /* Returns the canonical mask for a given mask,
+     removing values which are superseded by other values */
   static function getCanonicalMask($mask) {
     foreach (self::getData() as $value => $params) {
       if ($mask & $value) {

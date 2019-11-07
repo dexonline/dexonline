9545181172088bcbd6028b176452340fcf6553a4
Ladislau Strifler <ladislau.strifler@bbcomputer.ro>
2019-11-07 18:34:34 +0200

    Requested formatting and comments.

--- templates/bits/charmap.tpl
+++ templates/bits/charmap.tpl
@@ -1,81 +1,81 @@
 <div class="modal fade" tabindex="-1" role="dialog" id="modal-charmap">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <h4 class="modal-title">Inserează un șir de glife</h4>
-            </div>
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <h4 class="modal-title">Inserează un șir de glife</h4>
+      </div>
 
-            <div class="modal-body">
-              <div class="row">
-                <div class="row-modal" data-role="buttons"></div>
-                <div class="row-modal">
-                  <a role="button" id="editButton"
-                          class="btn btn-info collapsed" data-toggle="collapse"
-                          href="#editArea" aria-expanded="false"
-                          aria-controls="editArea">
-                    <span class="if-collapsed">Editează</span>
-                    <span class="if-not-collapsed">Închide</span>
-                  </a>
-                  <a role="button" id="textButton"
-                          class="btn btn-info collapsed pull-right" data-toggle="collapse"
-                          href="#charsArea" aria-expanded="false"
-                          aria-controls="charsArea">
-                    <span class="if-collapsed">mai multe glife</span>
-                    <span class="if-not-collapsed">o singură glifă</span>
-                  </a>
-                </div>
-                <div class="row-modal">
-                  <div id="editArea" class="collapse" aria-expanded="false">
-                    <textarea id="editBox" class="form-control" rows="10" title="Glife"></textarea>
-                    <div class="col">
-                      <div class="btn-group btn-group pull-right">
-                            <button type="submit" id="saveButton" class="btn btn-success"
-                                    title="salvează lista de glife">
-                              <i class="glyphicon glyphicon-floppy-disk"></i>
-                                <u>s</u>alvează
-                            </button>
-                            <button type="button" id="resetButton" class="btn btn-danger"
-                                    title="resetează lista la valorile inițiale">
-                              <i class="glyphicon glyphicon-refresh"></i>
-                              <u>r</u>esetează
-                            </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="row-modal">
-                  <div id="charsArea" class="collapse" aria-expanded="false">
-                    <div class="input-group">
-                      <div class="form-group has-feedback">
-                       <input type="text" class="form-control ui-autocomplete-input"
-                              name="charmapGlyphs" placeholder="adaugă glife"
-                              id="charsText" value="" maxlength="400" autocomplete="off">
-                       <span id="charsClear" class="glyphicon glyphicon-remove form-control-feedback"> </span>
-                       <span id="charsSend" class="glyphicon glyphicon-ok form-control-feedback collapse"> </span>
-                      </div>
-                      <span class="input-group-btn">
-                       <button type="button" value="trimite" id="charsInsert" class="btn btn-primary" data-dismiss="modal">
-                        <span class="glyphicon glyphicon-arrow-down"></span> Inserează
-                       </button>
-                      </span>
-                    </div>
-                  </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="row-modal" data-role="buttons"></div>
+          <div class="row-modal">
+            <a role="button" id="editButton"
+               class="btn btn-info collapsed" data-toggle="collapse"
+               href="#editArea" aria-expanded="false"
+               aria-controls="editArea">
+              <span class="if-collapsed">Editează</span>
+              <span class="if-not-collapsed">Închide</span>
+            </a>
+            <a role="button" id="textButton"
+               class="btn btn-info collapsed pull-right" data-toggle="collapse"
+               href="#charsArea" aria-expanded="false"
+               aria-controls="charsArea">
+              <span class="if-collapsed">mai multe glife</span>
+              <span class="if-not-collapsed">o singură glifă</span>
+            </a>
+          </div>
+          <div class="row-modal">
+            <div id="editArea" class="collapse" aria-expanded="false">
+              <textarea id="editBox" class="form-control" rows="10" title="Glife"></textarea>
+              <div class="col">
+                <div class="btn-group btn-group pull-right">
+                  <button type="submit" id="saveButton" class="btn btn-success"
+                          title="salvează lista de glife">
+                    <i class="glyphicon glyphicon-floppy-disk"></i>
+                    salvează
+                  </button>
+                  <button type="button" id="resetButton" class="btn btn-danger"
+                          title="resetează lista la valorile inițiale">
+                    <i class="glyphicon glyphicon-refresh"></i>
+                    resetează
+                  </button>
                 </div>
               </div>
             </div>
-
-            <div class="modal-footer text-muted text-left">
-                Aici puteți salva glife (semne) pe care le folosiți des și care sunt greu de tastat.</br>
-                Apăsați butonul <em>Editează</em> și personalizați lista, după modelul
-                <strong>m;M[;detaliere]</strong> pe câte o linie.</br>
-                Puteți chiar să salvați șiruri mai lungi de un caracter, dacă vă ajută.
+          </div>
+          <div class="row-modal">
+            <div id="charsArea" class="collapse" aria-expanded="false">
+              <div class="input-group">
+                <div class="form-group has-feedback">
+                  <input type="text" class="form-control ui-autocomplete-input"
+                         name="charmapGlyphs" placeholder="adaugă glife"
+                         id="charsText" value="" maxlength="400" autocomplete="off">
+                  <span id="charsClear" class="glyphicon glyphicon-remove form-control-feedback"> </span>
+                  <span id="charsSend" class="glyphicon glyphicon-ok form-control-feedback collapse"> </span>
+                </div>
+                <span class="input-group-btn">
+                  <button type="button" value="trimite" id="charsInsert" class="btn btn-primary" data-dismiss="modal">
+                    <span class="glyphicon glyphicon-arrow-down"></span> Inserează
+                  </button>
+                </span>
+              </div>
             </div>
-        </div> <!-- /.modal-content -->
-    </div> <!-- /.modal-dialog -->
+          </div>
+        </div>
+      </div>
+
+      <div class="modal-footer text-muted text-left">
+        Aici puteți salva glife (semne) pe care le folosiți des și care sunt greu de tastat.</br>
+        Apăsați butonul <em>Editează</em> și personalizați lista, după modelul
+        <strong>m;M[;detaliere]</strong> pe câte o linie.</br>
+        Puteți chiar să salvați șiruri mai lungi de un caracter, dacă vă ajută.
+      </div>
+    </div> <!-- /.modal-content -->
+  </div> <!-- /.modal-dialog -->
 </div>
 <script>
-    Charmap.init('#modal-charmap');
+  Charmap.init('#modal-charmap');
 </script>
--- www/js/charmap.js
+++ www/js/charmap.js
@@ -1,10 +1,13 @@
-(function(){
+(function () {
 
   // cookie definition
   var COOKIE = 'charmap';
 
-  // function handler for binding toggle
-  var HANDLER = null;
+  // signature of the showCharMap function in hotkeys.js
+  // passed as a handle in the show event, used for bind/unbind
+  // https://github.com/dexonline/dexonline/issues/844
+  var HANDLER = null; // set once during modal show
+
   var modalControls = {};
 
   var CYRILLIC = [
@@ -27,11 +30,11 @@
 
 
   // character read/edit logic
-  var Charmap = function() {
+  var Charmap = function () {
     this._cookie_json = $.cookie.json;
   };
 
-  Charmap.prototype.read = function() {
+  Charmap.prototype.read = function () {
     $.cookie.json = true;
     var cookie_value = $.cookie(COOKIE);
     var value = (cookie_value && cookie_value.length > 0) ? cookie_value : DEFAULT;
@@ -39,9 +42,9 @@
     return value;
   };
 
-  Charmap.prototype.edit = function(value) {
+  Charmap.prototype.edit = function (value) {
     $.cookie.json = true;
-    $.cookie(COOKIE, value, { expires: 36500, path: '/' });
+    $.cookie(COOKIE, value, {expires: 36500, path: '/'});
     $.cookie.json = this._cookie_json;
   };
 
@@ -54,18 +57,18 @@
 
   function changeButtonsCase(modal, shiftDown) {
     [].slice.call(modal.querySelectorAll('.btn-charmap'))
-      .forEach(function(button) {
+      .forEach(function (button) {
         var new_text = shiftDown ? button.getAttribute('data-upper') : button.getAttribute('data-lower');
-        button.innerText === new_text ? function(){}() : button.innerText = new_text;
+        button.innerText === new_text ? function () {}() : button.innerText = new_text;
         button.setAttribute('value', new_text);
       });
   }
 
   function listenForShiftChanged(modal) {
-    document.addEventListener('keydown', function(evt) {
+    document.addEventListener('keydown', function (evt) {
       isShiftKey(evt) && changeButtonsCase(modal, true);
     });
-    document.addEventListener('keyup',  function(evt) {
+    document.addEventListener('keyup', function (evt) {
       isShiftKey(evt) && changeButtonsCase(modal, false);
     });
   }
@@ -73,62 +76,60 @@
   // Insert character logic
   // (adapted from https://stackoverflow.com/questions/11076975/insert-text-into-textarea-at-cursor-position-javascript/41426040#41426040)
   function insertAtCursor(myField, myValue) {
-        // simple input/textarea field inserter
-		function _do_insert() {
-			var startPos = myField.selectionStart;
-			var endPos = myField.selectionEnd;
+    // simple input/textarea field inserter
+    function _do_insert() {
+      var startPos = myField.selectionStart;
+      var endPos = myField.selectionEnd;
 
-			myField.value = myField.value.substring(0, startPos)
-				+ myValue
-				+ myField.value.substring(endPos, myField.value.length);
+      myField.value = myField.value.substring(0, startPos)
+        + myValue
+        + myField.value.substring(endPos, myField.value.length);
 
-			var pos = startPos + myValue.length;
+      var pos = startPos + myValue.length;
 
-			myField.setSelectionRange(pos, pos);
-			myField.focus();
-		}
+      myField.setSelectionRange(pos, pos);
+      myField.focus();
+    }
 
-		//IE support
-		if (document.selection) {
-			myField.focus();
-			var sel = document.selection.createRange();
-			sel.text = myValue;
-		}
+    //IE support
+    if (document.selection) {
+      myField.focus();
+      var sel = document.selection.createRange();
+      sel.text = myValue;
+    }
 
-		// Microsoft Edge
-		else if(window.navigator.userAgent.indexOf("Edge") > -1) {
-			_do_insert();
-		}
+    // Microsoft Edge
+    else if (window.navigator.userAgent.indexOf("Edge") > -1) {
+      _do_insert();
+    }
 
-		//MOZILLA and others
-		else if (myField.selectionStart || myField.selectionStart === '0') {
-			_do_insert();
-		}
-
-		else {
-			myField.value += myValue;
-			myField.focus();
-		}
-	}
+    //MOZILLA and others
+    else if (myField.selectionStart || myField.selectionStart === '0') {
+      _do_insert();
+    } else {
+      myField.value += myValue;
+      myField.focus();
+    }
+  }
 
   function insertAtTinyMCECursor(editor, chr) {
-      // tinymce inserter
-      editor.insertContent(chr);
+    // tinymce inserter
+    editor.insertContent(chr);
   }
 
   function getInserter(target) {
     var is_tinymce = target.hasClass('mce-content-body');
     return (
       is_tinymce
-        ? function(chr) {
-          insertAtTinyMCECursor(tinymce.activeEditor, chr);
-        }
-        : function(chr) {
-          // target is a jQuery element,
-          // insertAtCursor requires a DOM element
-          // so we use .get(0).
-          insertAtCursor(target.get(0), chr);
-        }
+      ? function (chr) {
+        insertAtTinyMCECursor(tinymce.activeEditor, chr);
+      }
+    : function (chr) {
+      // target is a jQuery element,
+      // insertAtCursor requires a DOM element
+      // so we use .get(0).
+      insertAtCursor(target.get(0), chr);
+    }
     );
   }
 
@@ -153,25 +154,25 @@
 
     // Default properties
     var button = document.createElement('button');
-	  button.className = 'btn btn-default btn-charmap';
+    button.className = 'btn btn-default btn-charmap';
 
-	  button.innerText = lower;
+    button.innerText = lower;
 
-      button.setAttribute('title', title);
-	  button.setAttribute('data-lower', lower);
-	  button.setAttribute('data-upper', upper);
-	  button.setAttribute('value', lower);
+    button.setAttribute('title', title);
+    button.setAttribute('data-lower', lower);
+    button.setAttribute('data-upper', upper);
+    button.setAttribute('value', lower);
 
-	  button.addEventListener('click', function(){
-        setChars(button.getAttribute('value'));
-      });
+    button.addEventListener('click', function () {
+      setChars(button.getAttribute('value'));
+    });
 
-	  return button;
+    return button;
   }
 
-  function getButtonContent(config){
+  function getButtonContent(config) {
     return config.map(
-      function(entry) {
+      function (entry) {
         return isSection(entry) ? getSection(entry) : getButton(entry);
       });
   }
@@ -205,48 +206,50 @@
       charsClear: $('#charsClear', modal)
     };
 
-    modal.on('show.bs.modal', function() {
+    modal.on('show.bs.modal', function () {
       $(document).unbind('keydown', HANDLER);
       update();
     });
 
-    modal.on('hide.bs.modal', function(){
+    modal.on('hide.bs.modal', function () {
       inserter(modalControls.charsText.val());
       modalControls.charsText.val('');
     });
 
-    modal.on('hidden.bs.modal', function(){
+    modal.on('hidden.bs.modal', function () {
       $(document).bind('keydown', 'alt+q', HANDLER);
     });
 
-    modalControls.editButton.on('click', function() {
+    modalControls.editButton.on('click', function () {
       modalControls.textButton.toggleClass('disabled');
       modalControls.editBox.val(CHARMAP.read().join('\n'));
     });
 
-    modalControls.textButton.on('click', function() {
+    modalControls.textButton.on('click', function () {
       modalControls.editButton.toggleClass('disabled');
     });
 
-    modalControls.charsClear.on('click', function() {
+    modalControls.charsClear.on('click', function () {
       modalControls.charsText.val('');
     });
 
-    modalControls.charsText.on('change', function(e){
+    modalControls.charsText.on('change', function (e) {
       var isExpanded = modalControls.charsArea.attr('aria-expanded');
-      if (isExpanded === 'false') {modal.modal('hide');}
+      if (isExpanded === 'false') {
+        modal.modal('hide');
+      }
     });
 
-    modalControls.saveButton.on('click', function() {
+    modalControls.saveButton.on('click', function () {
       var value = modalControls.editBox.val();
-      var to_save = value.split(/\r\n|\r|\n/g).filter(function(val) {
+      var to_save = value.split(/\r\n|\r|\n/g).filter(function (val) {
         return val.trim() !== "";
       });
       CHARMAP.edit(to_save);
       update();
     });
 
-    modalControls.resetButton.on('click', function() {
+    modalControls.resetButton.on('click', function () {
       if (confirm('Confirmați resetarea glifelor la valorile inițiale?')) {
         $.removeCookie(COOKIE, {path: '/'});
         update();
@@ -257,21 +260,21 @@
     MODAL = modal;
   }
 
-	function show(insert_target, handler) {
-      if (MODAL) {
-        HANDLER = handler;
-        // update inserter global.
-        inserter = getInserter($(insert_target));
-        MODAL.modal();
-        MODAL.draggable({
-            handle: ".modal-header"
-        });
-      }
-	}
+  function show(insert_target, handler) {
+    if (MODAL) {
+      HANDLER = handler;
+      // update inserter global.
+      inserter = getInserter($(insert_target));
+      MODAL.modal();
+      MODAL.draggable({
+        handle: ".modal-header"
+      });
+    }
+  }
 
-	window.Charmap = {
-		show: show,
-        init: init
-	};
+  window.Charmap = {
+    show: show,
+    init: init
+  };
 
 })();

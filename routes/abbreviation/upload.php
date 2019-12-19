11d2e6f8b1dde68b7afad1e6c05082817849ccf6
Radu Borza <radu_borza@yahoo.it>
2019-12-19 06:31:54 +0200

    refactor upload page for Abbreviation and PageIndex

--- routes/abbreviation/upload.php
+++ routes/abbreviation/upload.php
@@ -1,112 +1,29 @@
 <?php
 User::mustHave(User::PRIV_ADMIN);
 
-$sourceId = Request::get('source');
-$saveButton = Request::has('saveButton');
-$cancelButton = Request::has('cancelButton');
-$delimiter = Request::get('delimiter') ?: '|';
-$csvFile = Request::getFile('file');
+define("MSG_SUCCESS" , "%d abrevieri au fost introduse în baza de date.");
 
-$userId = User::getActiveId();
-$csv = Session::get('csv', []);
+/* the function is needed by processPage and csv_to_objects from UploadUtils */
+function process_row($sourceId, $userId, $row) {
+  $abbrev = Abbreviation::create(
+    $sourceId,
+    trim($row['short']),
+    trim($row['internalRep']),
+    $row['ambiguous'],
+    $row['caseSensitive'],
+    $row['enforced'],
+    $userId
+  );
 
-try {
-  if ($csvFile) {
-    if ($csvFile['error'] !== UPLOAD_ERR_OK) {
-      throw new UploadException($csvFile['error']);
-    } else {
-      if ($csvFile['tmp_name'] != '') {
-        //getting an array for csv file contents
-        $csv = csv_to_array($csvFile['tmp_name'], $delimiter);
-
-        // stashing the array for saving operation
-        Session::set('csv', $csv);
-      }
-    }
-  }
-} catch (Exception $e) {
-  FlashMessage::add($e->getMessage());
+  return $abbrev;
 }
 
-if ($saveButton) {
-  if (count($csv) > 0) { // process the array only if we have some data
+$csv = UploadUtils::processPage();
 
-    $numSuccess = $numErrors = 0;
-    foreach ($csv as $row) { // handle each row
-      $abbrev = Abbreviation::create(
-        $sourceId, trim($row['short']), trim($row['internalRep']), $row['ambiguous'],
-        $row['caseSensitive'], $row['enforced'], $userId);
-
-      try {
-        $abbrev->save();
-        $numSuccess++;
-      } catch (Exception $e) {
-        FlashMessage::add($e->getMessage);
-        $numErrors++;
-      }
-    }
-
-    $message = "{$numSuccess} abrevieri au fost introduse în baza de date.";
-    $class = 'success';
-    if ($numErrors) {
-      $message .= " Alte {$numErrors} au generat erori.";
-      $class = 'warning';
-    }
-    FlashMessage::add($message, $class);
-  }
-  $cancelButton = true;
-} else {
-  if ($csvFile['name'] != '' && !FlashMessage::hasErrors()) {
-    $message = sprintf('Fișierul %s (%s linii) a fost încărcat.', $csvFile['name'], count($csv));
-    $class = FlashMessage::hasErrors() ? 'warning' : 'success';
-    FlashMessage::add($message, $class);
-  }
-}
-
-if ($cancelButton) {
-  Session::unsetVar('csv');
-  $csv = [];
-}
-
-// create Abbreviation objects so we can use the HtmlConverter
-$abbrevs = csv_to_objects($csv, $sourceId, $userId);
-
+// create Abbreviation objects so we can use the HtmlConverter and use them in smarty
 Smart::assign([
-  'abbrevs' => $abbrevs,
+  'abbrevs' => UploadUtils::csv_to_objects($csv, $sourceId, $userId),
   'modUser' => User::getActive(),
 ]);
 Smart::addResources('admin');
 Smart::display('abbreviation/upload.tpl');
-
-/*************************************************************************/
-
-function csv_to_array($filename = '', $delimiter = '|') {
-  $data = [];
-
-  if (file_exists($filename) && is_readable($filename)) {
-    $header = NULL;
-    if (($handle = fopen($filename, 'r')) !== FALSE) {
-      while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE) {
-        if (!$header) {
-          $header = $row;
-        } else {
-          $data[] = array_combine($header, $row);
-        }
-      }
-      fclose($handle);
-    }
-  }
-  return $data;
-}
-
-// converts an array of associative arrays to an array of Abbreviation objects.
-function csv_to_objects($csv, $sourceId, $userId) {
-  $results = [];
-  foreach ($csv as $row) {
-    $results[] = Abbreviation::create(
-      $sourceId, $row['short'], $row['internalRep'], $row['ambiguous'],
-      $row['caseSensitive'], $row['enforced'], $userId
-    );
-  }
-  return $results;
-}

ee235faa77b9a57676ff3425c88ffe2593fd65ac
Cătălin Frâncu <cata@francu.com>
2021-11-05 15:38:44 +0200

    Fix escaping error.

--- www/ajax/getSources.php
+++ www/ajax/getSources.php
@@ -4,11 +4,13 @@ require_once '../../lib/Core.php';
 User::mustHave(User::PRIV_STRUCT | User::PRIV_EDIT);
 
 $query = Request::get('term');
+$query = addslashes($query);
+
 // Latin alphabet comparisons - allow Ş or S instead of Ș
 $sources = Model::factory('Source')
-         ->where_raw("shortName collate utf8mb4_general_ci like '{$query}%'")
-         ->limit(10)
-         ->find_many();
+  ->where_raw("shortName collate utf8mb4_general_ci like '{$query}%'")
+  ->limit(10)
+  ->find_many();
 
 $resp = [ 'results' => [] ];
 foreach ($sources as $s) {

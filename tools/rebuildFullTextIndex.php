0bf90be249e82d4776131bdbed95ed6ed5d1f227
Cătălin Frâncu <cata@francu.com>
2021-11-21 23:49:20 +0200

    FullTextIndex: use a compacted array and qsort it.

--- tools/rebuildFullTextIndex.php
+++ tools/rebuildFullTextIndex.php
@@ -8,80 +8,9 @@ require_once __DIR__ . '/../lib/Core.php';
  * inserts much faster.
  */
 
-/**
- * Compares two numerically indexed arrays of equal lenghts.
- */
-function cmp(array $a, array $b) {
-  $i = 0;
-  while (($i < count($a)) && ($a[$i] == $b[$i])) {
-    $i++;
-  }
-
-  return ($i == count($a))
-    ? 0
-    : $a[$i] <=> $b[$i];
-}
-
 function getMemory($peak = false) {
   $m = $peak ? memory_get_peak_usage() : memory_get_usage();
   return round($m / 1048576);
-}
-
-/**
- * Bulk insert with hints from
- * https://dev.mysql.com/doc/refman/8.0/en/optimizing-innodb-bulk-data-loading.html
- */
-class BulkInsert {
-  private string $query = '';
-  private int $queryLen = 0; // avoid numerous strlen($this->query)
-  private int $queryLimit;
-  private int $records = 0;
-
-  function __construct() {
-    // Query length is limited only by max_packet_size. That could afford us
-    // millions of rows, but we'll put a reasonable cap on that.
-    $result = DB::getArrayOfRows('show variables like "max_allowed_packet"');
-    $maxPacketSize = (int)$result[0]['Value'];
-    $this->queryLimit = min($maxPacketSize * 0.9, 1000000);
-
-    DB::execute('SET unique_checks=0');
-    DB::execute('SET autocommit=0');
-  }
-
-  function push(array $row) {
-    $str = '(' . implode(',', $row) . ')';
-    if ($this->query) {
-      $str = ',' . $str;
-    }
-    $this->query .= $str;
-    $this->queryLen += strlen($str);
-    $this->records++;
-
-    if ($this->queryLen > $this->queryLimit) {
-      $this->run();
-    }
-  }
-
-  function run() {
-    if (!$this->query) {
-      return;
-    }
-
-    DB::execute('start transaction');
-    DB::execute(
-      'insert into FullTextIndex values ' . $this->query);
-    DB::execute('commit');
-    // Log::debug(sprintf('Inserted %d rows. | %d MB', $this->records, getMemory()));
-    $this->query = '';
-    $this->queryLen = 0;
-  }
-
-  function end() {
-    $this->run(); // call insert with remaining tuples
-
-    DB::execute('SET autocommit=1');
-    DB::execute('SET unique_checks=1');
-  }
 }
 
 /**
@@ -98,17 +27,13 @@ function buildInflectedFormMap() {
 
   foreach ($dbResult as $r) {
     $form = mb_strtolower($r['formNoAccent']);
-    $s = isset($ifMap[$form])
-      ? ($ifMap[$form] . ',')
-      : '';
-    $s .= $r['lexemeIds'];
-    $ifMap[$form] = $s;
+    $ifMap[$form] = ($ifMap[$form] ?? '') . ',' . $r['lexemeIds'];
   }
 
-  // The lexemeIds for each form are not necessarily unique due to (1)
-  // identical forms for different inflections and (2) upper/lowercase
-  // (Țuț/țuț). Make them unique.
+  // Make lexemeIds for each form unique. There may be duplicates due to (1)
+  // identical forms for different inflections and (2) upper/lowercase.
   foreach ($ifMap as $lexemeId => $str) {
+    $str = trim($str, ',');
     $parts = explode(',', $str);
     $parts = array_unique($parts);
     $ifMap[$lexemeId] = implode(',', $parts);
@@ -118,87 +43,132 @@ function buildInflectedFormMap() {
   return $ifMap;
 }
 
-function extractWords($s) {
-  // throw away hidden text
-  $s = preg_replace('/▶.*◀/sU', '', $s);
+/**
+ * Speeds up inserts by performing them in bulk.
+ */
+class BulkInsert {
+  private string $query = '';
+  private int $queryLimit;
+  private int $records = 0;
 
-  // throw away footnotes
+  function __construct() {
+    // Query length is limited only by max_packet_size. That could afford us
+    // millions of rows, but we'll put a reasonable cap on that.
+    $result = DB::getArrayOfRows('show variables like "max_allowed_packet"');
+    $maxPacketSize = (int)$result[0]['Value'];
+    $this->queryLimit = min($maxPacketSize * 0.9, 1000000);
+  }
+
+  function push(array $row) {
+    $this->query .= '(' . implode(',', $row) . '),';
+
+    if (++$this->records % 1000000 == 0) {
+      Log::info('%d rows pushed to BulkInsert');
+    }
+
+    if (strlen($this->query) > $this->queryLimit) {
+      $this->flush();
+    }
+  }
+
+  private function flush() {
+    if ($this->query) {
+      $s = trim($this->query, ',');
+      DB::execute('insert into FullTextIndex values ' . $s);
+      // Log::debug(sprintf('Inserted %d rows. | %d MB', $this->records, getMemory()));
+      $this->query = '';
+    }
+  }
+
+  function end() {
+    $this->flush(); // call insert with remaining data
+  }
+}
+
+function extractWords($s) {
+  // throw away hidden text and footnotes
+  $s = preg_replace('/▶.*◀/sU', '', $s);
   $s = preg_replace('/(?<!\\\\)\{\{.*\}\}/sU', '', $s);
 
   $s = mb_strtolower($s);
   $s = Str::removeAccents($s);
 
-  $words = preg_split('/\P{L}+/u', $s, null, PREG_SPLIT_NO_EMPTY);
-  return $words;
+  return preg_split('/\P{L}+/u', $s, null, PREG_SPLIT_NO_EMPTY);
+}
+
+function qsort(SplFixedArray $a, int $begin, int $end) {
+  $b = $begin;
+  $e = $end;
+  $pivot = $a[($begin + $end) / 2];
+
+  while ($b <= $e) {
+    while ($a[$b] < $pivot) {
+      $b++;
+    }
+    while ($a[$e] > $pivot) {
+      $e--;
+    }
+    if ($b <= $e) {
+      $aux = $a[$b]; $a[$b] = $a[$e]; $a[$e] = $aux;
+      $b++;
+      $e--;
+    }
+  }
+  if ($begin < $e) {
+    qsort($a, $begin, $e);
+  }
+  if ($b < $end) {
+    qsort($a, $b, $end);
+  }
 }
 
 /**
- * Reads all definitions and builds a map of
- * $lexemeId => concatenated ($definitionId, $position) tuples.
+ * Reads all definitions, creates the index and inserts it.
  */
-function buildInMemoryFti(array &$ifMap, array &$stopWordForms) {
+function scanDefinitions(array &$ifMap, array &$stopWordForms) {
+  $size = 0;  // number of records in the SplFixedArray
+  $limit = 0; // maximum size of the SplFixedArray
+  $defs = 0;  // number of definitions scanned
+  $spl = new SplFixedArray(0);
+
   $dbResult = DB::execute('select id, internalRep from Definition where status = 0');
-  $count = 0;
-  $map = [];
 
   foreach ($dbResult as $dbRow) {
     $words = extractWords($dbRow[1]);
 
     foreach ($words as $position => $word) {
-      if (!isset($stopWordForms[$word])) {
-        if (array_key_exists($word, $ifMap)) {
-          $lexemeIds = explode(',', $ifMap[$word]);
-          foreach ($lexemeIds as $lexemeId) {
-            $chunk = $dbRow[0] . ',' . $position;
-            if (isset($map[$lexemeId])) {
-              $map[$lexemeId] .= '|' . $chunk;
-            } else {
-              $map[$lexemeId] = $chunk;
-            }
+      if (!isset($stopWordForms[$word]) && isset($ifMap[$word])) {
+        $lexemeIds = explode(',', $ifMap[$word]);
+        foreach ($lexemeIds as $lexemeId) {
+          if ($size == $limit) {
+            $limit += 1000000;
+            $spl->setSize($limit);
           }
+          // 23 bits for definitionIds, 22 for lexemeIds, 16 for positions
+          $spl[$size++] = ($lexemeId << 38) | ($dbRow[0] << 16) | $position;
         }
       }
     }
 
-    if (++$count % 10000 == 0) {
+    if (++$defs % 50000 == 0) {
       $runTime = DebugInfo::getRunningTimeInMillis() / 1000;
-      $speed = round($count / $runTime);
-      Log::info('%d definitions scanned (%d defs/sec) | %d MB',
-                $count, $speed, getMemory());
+      $speed = round($defs / $runTime);
+      Log::info('%d definitions scanned (%d defs/sec) | index size %d | %d MB',
+                $defs, $speed, $size, getMemory());
     }
   }
-  Log::info("%d definitions scanned.", $count);
+  Log::info('%d definitions scanned | index size %d | %d MB', $defs, $size, getMemory());
+  $spl->setSize($size);
 
-  return $map;
-}
-
-function traverseInMemoryFti(array &$fti) {
+  Log::info('Sorting index.');
+  qsort($spl, 0, $size - 1);
+  Log::info('Index sorted.');
 
   $insert = new BulkInsert();
-
-  ksort($fti); // ensure lexemeId order
-  $count = 0;
-
-  foreach ($fti as $lexemeId => $str) {
-    $tuples = explode('|', $str);
-
-    $data = [];
-    foreach ($tuples as $tuple) {
-      $data[] = explode(',', $tuple);
-    }
-    usort($data, 'cmp'); // sort in primary key order
-    foreach ($data as $pair) {
-      $insert->push([ $lexemeId, $pair[0], $pair[1] ]);
-
-      if (++$count % 1000000 == 0) {
-        Log::info('%d rows merged from the in-memory FTI. | %d MB',
-                  $count, getMemory());
-      }
-    }
-
-    unset($fti[$lexemeId]);
+  foreach ($spl as $val)  {
+    // unpack triplets packed above
+    $insert->push([ $val >> 38, $val >> 16 & 0x3fffff, $val & 0xffff ]);
   }
-
   $insert->end();
 }
 
@@ -215,6 +185,8 @@ function main() {
   }
   Variable::poke(Variable::LOCK_FTI, '1');
   DB::execute('truncate table FullTextIndex');
+  DB::setBuffering(false);
+  DebugInfo::disable(); // to prevent logging of huge insert queries
 
   // build a map of stop words
   $stopWordForms = array_flip(DB::getArray(
@@ -225,8 +197,7 @@ function main() {
 
   // inflectedForm => concatenated lexemeIds
   $ifMap = buildInflectedFormMap();
-  $memFti = buildInMemoryFti($ifMap, $stopWordForms);
-  traverseInMemoryFti($memFti);
+  scanDefinitions($ifMap, $stopWordForms);
 
   Variable::clear(Variable::LOCK_FTI);
   Log::notice('finished; peak memory usage %d MB', getMemory(true));

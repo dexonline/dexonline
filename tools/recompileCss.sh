c3eaf5ac7c4233b5cff5cc7a33771f94be7c66f7
Cătălin Frâncu <cata@francu.com>
2021-12-22 11:04:48 +0200

    comments and stuff.

--- tools/recompileCss.sh
+++ tools/recompileCss.sh
@@ -18,24 +18,27 @@ sassc -t $TYPE $INPUT > $OUTPUT
 
 # 2. Compile the expanded light theme
 INPUT=www/scss/main-light.scss
-OUTPUT=/tmp/bootstrap-light.min.css
+OUTPUT=/tmp/bootstrap-light.css
 TYPE=expanded
 echo "Compiling $INPUT into $OUTPUT ($TYPE)"
 sassc -t $TYPE $INPUT > $OUTPUT
 
 # 3. Compile the expanded dark theme
 INPUT=www/scss/main-dark.scss
-OUTPUT=/tmp/bootstrap-dark.min.css
+OUTPUT=/tmp/bootstrap-dark.css
 TYPE=expanded
 echo "Compiling $INPUT into $OUTPUT ($TYPE)"
 sassc -t $TYPE $INPUT > $OUTPUT
 
 # 4. Diff the expanded files
-FROM=/tmp/bootstrap-light.min.css
-TO=/tmp/bootstrap-dark.min.css
+FROM=/tmp/bootstrap-light.css
+TO=/tmp/bootstrap-dark.css
 OUTPUT=www/css/third-party/bootstrap-diff.css
 echo "Diffing $FROM and $TO into $OUTPUT"
 php tools/cssDiff.php -f $FROM -t $TO -p 'html.dark' -r -w > $OUTPUT
+
+# Note: do not minify bootstrap-diff.css. It saves 4 KB in unzipped form, but
+# nothing in zipped form.
 
 # 5. Cleanup
 rm $FROM $TO

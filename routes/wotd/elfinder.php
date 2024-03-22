23cedd24f7d6c8c9e73967f4e156046d13fa150d
Radu Borza <horia.borza@payu.ro>
2024-03-22 18:59:21 +0200

    1st test for elfinder multiroot

--- routes/wotd/elfinder.php
+++ routes/wotd/elfinder.php
@@ -2,7 +2,8 @@
 
 require_once __DIR__ . '/../../lib/third-party/elfinder/autoload.php';
 
-$opts = ElfinderUtil::getOptions('img/wotd/', 'Imagini cuvântul zilei');
+#$opts = ElfinderUtil::getOptions('img/wotd/', 'Imagini cuvântul zilei');
+$opts = ElfinderUtil::getOptionsMultiRoot();
 
 // run elFinder
 $connector = new elFinderConnector(new elFinder($opts));

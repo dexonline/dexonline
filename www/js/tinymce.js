69780d88058a6212cde51dec574f9f23fa83f6ce
Radu Borza <horia.borza@payu.ro>
2024-06-21 03:41:55 +0300

    TinyMCE small caps

--- www/js/tinymce.js
+++ www/js/tinymce.js
@@ -159,7 +159,7 @@ $(function() {
     s = s.replace(/%([^%]*)%/g, '<span class="spaced">$1</span>');
     s = s.replace(/~~~SAVE~~~/g, '\\%'); // restore \%
 
-    s = s.replace(/\{\~([^~]*)\~\}/g, '<span class="small-caps">$1</span>');
+    s = s.replace(/\{~(.*?)~\}/g, '<span class="small-caps">$1</span>');
 
     s = s.replace(/\\#/g, '~~~SAVE~~~'); // move \# out of the way
     s = s.replace(/#([^#]*)#/g, '<abbr>$1</abbr>');

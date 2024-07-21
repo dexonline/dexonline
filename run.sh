e889aa31d6a95976ef143b1271f646715fc9e31c
Foaieee <foaie_cristianconstantin@yahoo.com>
2024-07-21 15:30:05 +0300

    fix config DATABASE and URL_PREFIX

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ docker-compose up -d
 
 docker-compose exec db bash init.sh
 docker-compose exec app bash tools/setup.sh
+docker-compose exec app bash -c "sed -i 's|const DATABASE = .*;|const DATABASE = '\''mysql://root:admin@db/dexonline'\'';|' Config.php"
+docker-compose exec app bash -c "sed -i 's|const URL_PREFIX = .*;|const URL_PREFIX = '\''/'\'';|' Config.php"

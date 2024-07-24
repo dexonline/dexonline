86ea49b87419e1b37828073890ac57f78ebce11e
Foaieee <foaie_cristianconstantin@yahoo.com>
2024-07-24 15:49:08 +0300

    move docker related file in docker directory

---
+++ docker/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+docker-compose build
+docker-compose up -d
+
+docker-compose exec db bash init.sh
+
+docker-compose exec app bash tools/setup.sh
+docker-compose exec app bash -c "touch /var/log/dexonline.log"
+docker-compose exec app bash -c "chmod 666 /var/log/dexonline.log"
+docker-compose exec app bash -c "sed -i 's|const DATABASE = .*;|const DATABASE = '\''mysql://root:admin@db/dexonline'\'';|' Config.php"
+docker-compose exec app bash -c "sed -i 's|const URL_PREFIX = .*;|const URL_PREFIX = '\''/'\'';|' Config.php"

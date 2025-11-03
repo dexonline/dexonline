bf1578c663842ba012c96f0031d3907c5216fa1f
Radu Borza <horia.borza@payu.ro>
2025-11-04 00:52:37 +0200

    allow 50 video clips on pronunciation tab

--- lib/models/Subtitle.php
+++ lib/models/Subtitle.php
@@ -3,13 +3,15 @@
 class Subtitle extends BaseObject {
   public static $_table = 'Subtitle';
 
+  const LIMIT_SUBS = 50;
+
   static function LoadSubtitles($word) {
     return Model::factory('Subtitle')
       ->join('VideoClip', 'VideoClip.id = Subtitle.clipId')
       ->select('VideoClip.videoId', 'id')
       ->select('Subtitle.start')
       ->whereEqual('Subtitle.word', $word)
-      ->limit(20)
+      ->limit(Subtitle::LIMIT_SUBS)
       ->find_array();
   }
 

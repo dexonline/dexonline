84d4c1dbafbf74d6549e9e96028a0f08310c6e80
Radu Borza <horia.borza@payu.ro>
2026-09-25 03:47:58 +0300

    dashboard ergonomics

--- routes/report/unassociatedDefinitions.php
+++ routes/report/unassociatedDefinitions.php
@@ -20,11 +20,12 @@ if ($associateButton) {
   Util::redirectToSelf();
 }
 
+$notToCheck = [Definition::ST_DELETED, Definition::ST_REVIEW];
 $defs = Model::factory('Definition')
       ->table_alias('d')
       ->select('d.*')
       ->left_outer_join('EntryDefinition', ['d.id', '=', 'ed.definitionId'], 'ed')
-      ->where_not_in('d.status', [Definition::ST_DELETED, Definition::ST_REVIEW])
+      ->where_not_in('d.status', $notToCheck)
       ->where_null('ed.id')
       ->find_many();
 
